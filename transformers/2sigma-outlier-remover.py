1f55bccc2ff153c7ea8ca9acd8df62adae80f053
Arno Candel <arno.candel@gmail.com>
2019-03-08 00:34:05 -0800

    Add two-sigma outlier remover

---
+++ transformers/2sigma-outlier-remover.py
@@ -0,0 +1,25 @@
+from h2oaicore.transformer_utils import CustomTransformer
+from h2oaicore.systemutils import config
+import datatable as dt
+import numpy as np
+import h2o
+import uuid
+from h2o.estimators.deeplearning import H2OAutoEncoderEstimator
+
+
+class MyTwoSigmaOutlierRemoverTransformer(CustomTransformer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.mean = None
+        self.sd = None
+
+    def fit_transform(self, X: dt.Frame, y: np.array = None):
+        self.mean = X.mean1()
+        self.sd = X.sd1()
+        return self.transform(X)
+
+    def transform(self, X: dt.Frame):
+        X = dt.Frame(X)
+        X[self.mean - 2 * self.sd > dt.f[0], :] = self.mean - 2 * self.sd
+        X[self.mean + 2 * self.sd < dt.f[0], :] = self.mean + 2 * self.sd
+        return X
