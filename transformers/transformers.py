29564bc052e7633cfefe23656d97d6aa8f428fec
Arno Candel <arno.candel@gmail.com>
2019-03-29 16:28:44 -0700

    Seed random transformer.

--- transformers/transformers.py
+++ transformers/transformers.py
@@ -89,8 +89,14 @@ class MySegfaultTransformer(CustomTransformer):
 
 
 class MyRandomTransformer(CustomTransformer):
+    def __init__(self, seed=12345, **kwargs):
+        super().__init__(**kwargs)
+        self.seed = seed
+
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         return self.transform(X)
 
     def transform(self, X: dt.Frame):
+        np.random.seed(self.seed)
         return np.random.rand(*X.shape)
+
