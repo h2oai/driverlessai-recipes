753f96e9b19e0b3ac0d95ec7033bcf84a83a799c
Arno Candel <arno.candel@gmail.com>
2019-04-02 18:15:18 -0700

    Add separate files for all transformers, remove transformers.py

---
+++ transformers/strlen_transformer.py
@@ -0,0 +1,16 @@
+from h2oaicore.transformer_utils import CustomTransformer
+import datatable as dt
+import numpy as np
+
+
+class MyStrLenEncoderTransformer(CustomTransformer):
+    @staticmethod
+    def get_default_properties():
+        return dict(col_type="categorical", min_cols=1, max_cols=1, relative_importance=1)
+
+    def fit_transform(self, X: dt.Frame, y: np.array = None):
+        return self.transform(X)
+
+    def transform(self, X: dt.Frame):
+        return X.to_pandas().astype(str).iloc[:, 0].str.len()
+
