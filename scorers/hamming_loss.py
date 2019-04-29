2723f045eb558ed9fcba19d81dd0074f67e4ac23
Arno Candel <arno.candel@gmail.com>
2019-04-29 16:46:14 -0700

    Add label encoding for hamming loss.

--- scorers/hamming_loss.py
+++ scorers/hamming_loss.py
@@ -2,6 +2,7 @@ import typing
 import numpy as np
 from h2oaicore.metrics import CustomScorer
 from sklearn.metrics import hamming_loss
+from sklearn.preprocessing import LabelEncoder
 
 
 class MyHammingLoss(CustomScorer):
@@ -16,5 +17,6 @@ class MyHammingLoss(CustomScorer):
               predicted: np.array,
               sample_weight: typing.Optional[np.array] = None,
               labels: typing.Optional[np.array] = None) -> float:
+        actual = LabelEncoder().fit(labels).transform(actual)
         predicted = np.argmax(predicted, axis=1)
         return hamming_loss(actual, predicted, labels, sample_weight)
