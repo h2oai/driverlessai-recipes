87ecd357087ef4ad23d0624b78432c0bfcc14500
Arno Candel <arno.candel@gmail.com>
2019-04-30 17:27:01 -0700

    Add H2O-3 GBM custom model.

---
+++ models/h2o-3-gbm.py
@@ -0,0 +1,92 @@
+from h2oaicore.models import CustomModel
+import h2o
+import uuid
+import os
+from h2o.estimators.gbm import H2OGradientBoostingEstimator
+
+
+class H2OGBMModel(CustomModel):
+    _regression = True
+    _binary = True
+    _multiclass = True
+
+    _boosters = ['h2ogbm']
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.id = str(uuid.uuid4())[:10]
+        self.raw_model_bytes = None
+        self.target = "__target__"
+
+    def fit(self, X, y, sample_weight=None, eval_set=None, sample_weight_eval_set=None, **kwargs):
+        h2o.init()
+        model_path = None
+
+        orig_cols = list(X.names)
+        train_X = h2o.H2OFrame(X.to_pandas())
+        train_y = h2o.H2OFrame(y, column_names=[self.target])
+        train_frame = train_X.cbind(train_y)
+        valid_frame = None
+        valid_X = None
+        valid_y = None
+        model = None
+        if eval_set is not None:
+            valid_X = h2o.H2OFrame(eval_set[0][0].to_pandas())
+            valid_y = h2o.H2OFrame(eval_set[0][1], column_names=[self.target])
+            valid_frame = valid_X.cbind(valid_y)
+
+        try:
+            model = H2OGradientBoostingEstimator()
+            model.train(x=train_X.names,
+                        y=self.target,
+                        training_frame=train_frame,
+                        validation_frame=valid_frame,
+                        model_id=self.id)
+            model_path = h2o.save_model(model=model)
+            with open(model_path, "rb") as f:
+                self.raw_model_bytes = f.read()
+
+        finally:
+            if model_path is not None:
+                os.remove(model_path)
+            for xx in [train_frame, train_X, train_y, model, valid_frame, valid_X, valid_y]:
+                if xx is not None:
+                    h2o.remove(xx)
+
+        # need to move to wrapper
+        self.feature_names_fitted = orig_cols
+        self.transformed_features = self.feature_names_fitted
+        self.best_ntree_limit = model.params['ntrees']['actual']
+        # must always set best_iterations
+        self.best_iterations = self.best_ntree_limit + 1
+
+        df_varimp = model.varimp(True)
+        df_varimp.index = df_varimp['variable']
+        df_varimp = df_varimp.iloc[:, 1]  # relative importance
+        df_varimp = df_varimp[self.feature_names_fitted]  # order by fitted features
+        self.set_feature_importances(df_varimp.values)
+        return self
+
+    def predict(self, X, **kwargs):
+        h2o.init()
+        with open(self.id, "wb") as f:
+            f.write(self.raw_model_bytes)
+        model = h2o.load_model(self.id)
+        os.remove(self.id)
+        test_frame = h2o.H2OFrame(X.to_pandas())
+        preds_frame = None
+
+        pred_contribs = kwargs.get('pred_contribs', None)
+        output_margin = kwargs.get('output_margin', None)
+
+        try:
+            if not pred_contribs:
+                preds_frame = model.predict(test_frame)
+                return preds_frame.as_data_frame(header=False).values
+            else:
+                raise NotImplementedError("H2O-3 has Shapley, just needs to be implemented")
+        finally:
+            h2o.remove(self.id)
+            h2o.remove(test_frame)
+            if preds_frame is not None:
+                h2o.remove(preds_frame)
