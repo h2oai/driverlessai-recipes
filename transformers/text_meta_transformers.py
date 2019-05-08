7921bbf24b0fa81315bce04cebce2402aa25be50
Arno Candel <arno.candel@gmail.com>
2019-05-08 10:14:50 -0700

    Code simplification.

--- transformers/text_meta_transformers.py
+++ transformers/text_meta_transformers.py
@@ -3,7 +3,8 @@ import datatable as dt
 import numpy as np
 import string
 
-class CountWordsTransformer(CustomTransformer):
+
+class WordBaseTransformer:
     @staticmethod
     def get_default_properties():
         return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
@@ -11,82 +12,34 @@ class CountWordsTransformer(CustomTransformer):
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         return self.transform(X)
 
+class CountWordsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len(x.split()))
 
-class CountUniqueWordsTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class CountUniqueWordsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len(set(x.split())))
 
-class CountUpperWordsTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class CountUpperWordsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len([w for w in x.split() if w.isupper()]))
 
-class CountNumericWordsTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class CountNumericWordsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len([w for w in x.split() if w.isnumeric()]))
 
-class CountUpperCharsTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class CountUpperCharsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len([c for c in x if c.isupper()]))
 
-class CountNumericCharsTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class CountNumericCharsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len([c for c in x if c.isnumeric()]))
 
-class CountPunctCharsTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class CountPunctCharsTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: len([c for c in x if c in string.punctuation]))
 
-class MeanWordLengthTransformer(CustomTransformer):
-    @staticmethod
-    def get_default_properties():
-        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
-
-    def fit_transform(self, X: dt.Frame, y: np.array = None):
-        return self.transform(X)
-
+class MeanWordLengthTransformer(WordBaseTransformer, CustomTransformer):
     def transform(self, X: dt.Frame):
         return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
