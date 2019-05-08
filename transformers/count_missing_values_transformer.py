1e95a0000a9cec3e8f69f2459d0258104d226226
Arno Candel <arno.candel@gmail.com>
2019-05-08 12:44:45 -0700

    Adds missing values count per row.

---
+++ transformers/count_missing_values_transformer.py
@@ -0,0 +1,26 @@
+from h2oaicore.transformer_utils import CustomTransformer
+import datatable as dt
+import numpy as np
+
+
+class CountMissingPerRowTransformer(CustomTransformer):
+    @staticmethod
+    def get_default_properties():
+        return dict(col_type="all", min_cols="all", max_cols="all", relative_importance=1)
+
+    def fit_transform(self, X: dt.Frame, y: np.array = None):
+        return self.transform(X)
+
+    def transform(self, X: dt.Frame):
+        if X.ncols == 0:
+            return np.zeros(X.nrows)
+        return X[:, dt.sum([dt.isna(dt.f[x]) for x in range(X.ncols)])]
+
+class CountMissingNumericsPerRowTransformer(CountMissingPerRowTransformer):
+    def transform(self, X: dt.Frame):
+        return super().transform(X[:, [int, float]])
+
+class CountMissingStringsPerRowTransformer(CountMissingPerRowTransformer):
+    def transform(self, X: dt.Frame):
+        return super().transform(X[:, [str]])
+
