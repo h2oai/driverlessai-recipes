d65f9863831f5e50c0a067ce8c27dbdf0f80132a
Jonathan C. McKinney <pseudotensor@gmail.com>
2019-05-17 13:37:35 -0700

    Fix fuzzy recipe to handle global import of nltk since no longer in DAI requirements

--- transformers/fuzzy_text_similarity_transformers.py
+++ transformers/fuzzy_text_similarity_transformers.py
@@ -3,7 +3,10 @@
 from h2oaicore.transformer_utils import CustomTransformer
 import datatable as dt
 import numpy as np
+
+_global_modules_needed_by_name = ['nltk==3.4.1']
 import nltk
+
 
 class FuzzyBaseTransformer:
     _modules_needed_by_name = ['fuzzywuzzy==0.17.0']
@@ -21,8 +24,8 @@ class FuzzyBaseTransformer:
         method = getattr(fuzz, self.__class__._method)
         output = []
         X = X.to_pandas()
-        text1_arr = X.iloc[:,0].values
-        text2_arr = X.iloc[:,1].values
+        text1_arr = X.iloc[:, 0].values
+        text2_arr = X.iloc[:, 1].values
         for ind, text1 in enumerate(text1_arr):
             try:
                 text1 = str(text1).lower().split()
@@ -34,20 +37,26 @@ class FuzzyBaseTransformer:
                 output.append(-1)
         return np.array(output)
 
+
 class FuzzyQRatioTransformer(FuzzyBaseTransformer, CustomTransformer):
     _method = "QRatio"
+
 
 class FuzzyWRatioTransformer(FuzzyBaseTransformer, CustomTransformer):
     _method = "WRatio"
 
+
 class FuzzyPartialRatioTransformer(FuzzyBaseTransformer, CustomTransformer):
     _method = "partial_ratio"
+
 
 class FuzzyTokenSetRatioTransformer(FuzzyBaseTransformer, CustomTransformer):
     _method = "token_set_ratio"
 
+
 class FuzzyTokenSortRatioTransformer(FuzzyBaseTransformer, CustomTransformer):
     _method = "token_sort_ratio"
+
 
 class FuzzyPartialTokenSortRatioTransformer(FuzzyBaseTransformer, CustomTransformer):
     _method = "partial_token_sort_ratio"
