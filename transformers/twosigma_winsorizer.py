a2e926a44b291e4c565d50d78edb901a0d94aa99
Jonathan C. McKinney <pseudotensor@gmail.com>
2019-05-20 00:43:05 -0700

    Fix twosigma winsorizer transformer

--- transformers/twosigma_winsorizer.py
+++ transformers/twosigma_winsorizer.py
@@ -4,6 +4,10 @@ import numpy as np
 
 
 class MyTwoSigmaWinsorizer(CustomTransformer):
+    @staticmethod
+    def get_default_properties():
+        return dict(col_type="numeric", min_cols=1, max_cols=1, relative_importance=1)
+
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         self.mean = X.mean1()
         self.sd = X.sd1()
@@ -11,6 +15,6 @@ class MyTwoSigmaWinsorizer(CustomTransformer):
 
     def transform(self, X: dt.Frame):
         X = dt.Frame(X)
-        X[self.mean - 2 * self.sd > dt.f[0], :] = self.mean - 2 * self.sd
-        X[self.mean + 2 * self.sd < dt.f[0], :] = self.mean + 2 * self.sd
+        X[self.mean - 2 * self.sd > dt.f[0], float] = self.mean - 2 * self.sd
+        X[self.mean + 2 * self.sd < dt.f[0], float] = self.mean + 2 * self.sd
         return X
