d5dfb2aeff3741a81324b330ba134541e2bec37e
Arno Candel <arno.candel@gmail.com>
2019-05-21 17:32:24 -0700

    Cosmetics.

--- transformers/general_time_series_transformer.py
+++ transformers/general_time_series_transformer.py
@@ -6,19 +6,19 @@ import numpy as np
 class GeneralTimeSeriesTransformer(CustomTimeSeriesTransformer):
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         # FIXME - use the following attributes
-        # self._encoder
-        # self._tgc
-        # self._pred_gap
-        # self._pred_periods
-        # self._lag_sizes
-        # self._lag_feature
-        # self._target
-        # self._tsp
-        # self._time_column
-        # self._datetime_formats
-        self._encoder.fit(X.to_pandas())
+        # self.encoder
+        # self.tgc
+        # self.pred_gap
+        # self.pred_periods
+        # self.lag_sizes
+        # self.lag_feature
+        # self.target
+        # self.tsp
+        # self.time_column
+        # self.datetime_formats
+        self.encoder.fit(X.to_pandas())
         return self.transform(X)
 
     def transform(self, X: dt.Frame):
-        return self._encoder.transform(X.to_pandas())
+        return self.encoder.transform(X.to_pandas())
 
