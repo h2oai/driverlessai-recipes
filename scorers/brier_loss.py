2bc9b9112ec132fd527056250b29c449308da517
vopani <rohanrao88@gmail.com>
2019-05-23 17:19:37 -0700

    brier loss

---
+++ scorers/brier_loss.py
@@ -0,0 +1,21 @@
+import typing
+import numpy as np
+from h2oaicore.metrics import CustomScorer
+from sklearn.metrics import brier_score_loss
+from sklearn.preprocessing import LabelEncoder
+
+
+class MyBrierLoss(CustomScorer):
+    _description = "Brier Score Loss"
+    _binary = True
+    _maximize = False
+    _perfect_score = 0
+    _display_name = "BRIER"
+
+    def score(self,
+              actual: np.array,
+              predicted: np.array,
+              sample_weight: typing.Optional[np.array] = None,
+              labels: typing.Optional[np.array] = None) -> float:
+        actual = LabelEncoder().fit_transform(actual)
+        return brier_score_loss(actual, predicted, sample_weight)
