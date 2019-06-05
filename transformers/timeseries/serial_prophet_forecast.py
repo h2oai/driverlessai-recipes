34c21b70bfe680dd8b2a345807506b314953e314
goldentom42 <olivier.grellier@h2o.ai>
2019-06-05 22:09:57 +0200

    fix predictions order for prophet. fbprophet returns predcitions ordered by date so different from input frame.

--- transformers/timeseries/serial_prophet_forecast.py
+++ transformers/timeseries/serial_prophet_forecast.py
@@ -34,7 +34,7 @@ class MySerialProphetTransformer(CustomTimeSeriesTransformer):
 
     @staticmethod
     def is_enabled():
-        return False
+        return True
 
     @staticmethod
     def get_default_properties():
@@ -86,7 +86,21 @@ class MySerialProphetTransformer(CustomTimeSeriesTransformer):
             if grp_hash in self.models:
                 model = self.models[grp_hash]
                 if model is not None:
-                    XX = model.predict(X)[['yhat']]
+                    # Facebook Prophet returns the predictions ordered by time
+                    # So we should keep track of the times for each group so that
+                    # predictions are ordered the same as the imput frame
+                    # Make a copy of the input dates
+                    X_ds = X.copy()
+                    X_ds['ds'] = pd.to_datetime(X_ds['ds'])
+                    # Predict with prophet, get the time and prediction and index by time as well
+                    # In the case date repeats inside of a group (this happens at least in acceptance test)
+                    # We groupby date and keep the max (prophet returns the same value for a given date)
+                    # XX will contain the predictions indexed by date
+                    XX = model.predict(X)[['ds', 'yhat']].groupby('ds').max()
+                    # Now put yhat in the right order, simply by maping the dates to the predictions
+                    X_ds['yhat'] = X_ds["ds"].map(XX['yhat'])
+                    # Now set XX back to the predictions and drop the index
+                    XX = X_ds['yhat'].reset_index(drop=True)
                 else:
                     XX = pd.DataFrame(np.full((X.shape[0], 1), self.nan_value), columns=['yhat'])  # invalid model
             else:
