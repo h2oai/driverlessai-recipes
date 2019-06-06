4bf3a1fe5728e49f7892680385e46e4b65b67440
goldentom42 <olivier.grellier@h2o.ai>
2019-06-06 14:38:52 +0200

    Faster version of the model using groupby for fit and merge for predict. DT code also available

--- models/historic_mean.py
+++ models/historic_mean.py
@@ -15,7 +15,7 @@ class HistoricMeanModel(CustomTimeSeriesModel):
 
     @staticmethod
     def do_acceptance_test():
-        return False
+        return True
 
     @staticmethod
     def is_enabled():
@@ -29,43 +29,64 @@ class HistoricMeanModel(CustomTimeSeriesModel):
         self.means = {}
         if self.tgc is None or not all([x in X.names for x in self.tgc]):
             return
+
+        if self.time_column is None:
+            self.time_column = self.tgc[0]
+
+        tgc_wo_time = list(np.setdiff1d(self.tgc, self.time_column))
+
+        # Datatable code
+        #
+        # if len(tgc_wo_time) > 0:
+        #     self.nan_value = np.mean(y)
+        #     self.ntrain = X.shape[0]
+        #     X_dt = X.copy()
+        #     X_dt.cbind(dt.Frame({"y": y}))
+        #     self.group_means = X_dt[:, dt.mean(dt.f.y), dt.by(*tgc_wo_time)]
+        #     # Have meaningful column names
+        #     self.group_means.names = tgc_wo_time + ["yhat"]
+        # else:
+        #     self.group_means = np.mean(y)
+
+        # Pandas code
         XX = X[:, self.tgc].to_pandas()
         XX['y'] = np.array(y)
-        self.nan_value = np.mean(y)
-        self.ntrain = X.shape[0]
-        tgc_wo_time = list(np.setdiff1d(self.tgc, self.time_column))
         if len(tgc_wo_time) > 0:
-            XX_grp = XX.groupby(tgc_wo_time)
+            self.nan_value = np.mean(y)
+            self.ntrain = X.shape[0]
+            self.group_means = XX.groupby(tgc_wo_time)["y"].mean().reset_index()
+            # Have meaningful column names
+            self.group_means.columns = tgc_wo_time + ["yhat"]
         else:
-            XX_grp = [([None], XX)]
-        for key, X in XX_grp:
-            key = key if isinstance(key, list) else [key]
-            grp_hash = '_'.join(map(str, key))
-            self.means[grp_hash] = X['y'].mean()
+            self.group_means = np.mean(y)
 
     def predict(self, X, **kwargs):
         if self.tgc is None or not all([x in X.names for x in self.tgc]):
             return np.ones(X.shape[0]) * self.nan_value
-        XX = X[:, self.tgc].to_pandas()
+
         tgc_wo_time = list(np.setdiff1d(self.tgc, self.time_column))
+
+        # Datatable code
+        # if len(tgc_wo_time) > 0:
+        #     # Join the average per group to the input datafrane
+        #     self.group_means.key = tgc_wo_time
+        #     # Predictions for unknown tgc are valued with 0s
+        #     # Waiting for issue https://github.com/h2oai/datatable/issues/1872
+        #     return X[:, :, dt.join(self.group_means)][:, "yhat"].to_numpy()[:, 0]
+        # else:
+        #     # if no Groups are avaible then just return the target average
+        #     return np.full((X.shape[0], 1), self.nan_value)
+
+        # Pandas code
+        XX = X[:, self.tgc].to_pandas()
         if len(tgc_wo_time) > 0:
-            XX_grp = XX.groupby(tgc_wo_time)
+            # Join the average per group to the input datafrane
+            return XX[tgc_wo_time].merge(
+                right=self.group_means,
+                on=tgc_wo_time,
+                how='left'
+            )["yhat"].fillna(self.nan_value).values
+
         else:
-            XX_grp = [([None], XX)]
-        preds = []
-        for key, X in XX_grp:
-            key = key if isinstance(key, list) else [key]
-            grp_hash = '_'.join(map(str, key))
-            if grp_hash in self.means:
-                mean = self.means[grp_hash]
-                if mean is not None:
-                    yhat = np.ones(X.shape[0]) * mean
-                    XX = pd.DataFrame(yhat, columns=['yhat'])
-                else:
-                    XX = pd.DataFrame(np.full((X.shape[0], 1), self.nan_value), columns=['yhat'])  # invalid model
-            else:
-                XX = pd.DataFrame(np.full((X.shape[0], 1), self.nan_value), columns=['yhat'])  # unseen groups
-            XX.index = X.index
-            preds.append(XX)
-        XX = pd.concat(tuple(preds), axis=0).sort_index()
-        return XX.values
+            # if no Groups are avaible then just return the target average
+            return np.full((X.shape[0], 1), self.nan_value)
