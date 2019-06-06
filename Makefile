f322f7445ba51293ed82e0fd9b73a3193aa50810
Arno Candel <arno.candel@gmail.com>
2019-06-05 17:24:05 -0700

    Update

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: README.md
+
+README.md:
+	$(shell ./gen.sh)
--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
-       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' | grep -v '.pyc')
+       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' | grep -v '.pyc' | grep -v 'LICENSE' | grep -v 'Makefile' | grep -v 'README.md' | grep -v 'gen.sh' | grep -v '^.$')
 printf "# Recipes for H2O Driverless AI\n\n${tree}" > README.md
