1b33818b747e12b829f662772d1b97bb34e6ef8f
Arno Candel <arno.candel@gmail.com>
2019-06-20 14:00:45 -0700

    Use gen.py to create README.

---
+++ gen.py
@@ -0,0 +1,13 @@
+exclude = ['.', '.git', 'data', 'Makefile', 'LICENSE', 'README.md', 'gen.sh', 'gen.py']
+import os
+print("# Recipes for H2O Driverless AI\n")
+for dirpath, dirs, files in os.walk("."):
+    if all(x not in dirpath for x in exclude if len(x) > 1):
+        path = dirpath.split('/')
+        pdir = os.path.basename(dirpath)
+        if pdir not in exclude:
+            print(' ' * (len(path) - 2) + "* " + "[" + pdir + "](" + dirpath + ")")
+            for f in files:
+                if f not in exclude:
+                    if f[-3:] == ".py":
+                        print(' ' * (len(path)) + "* [" + f + "](" + dirpath + "/" + f + ")")
--- gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-tree=$(tree -f --noreport -I '*~' --charset ascii $1 |
-       sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' | grep -v '.pyc' | grep -v 'LICENSE' | grep -v 'Makefile' | grep -v 'README.md' | grep -v 'gen.sh' | grep -v '^.$')
-printf "# Recipes for H2O Driverless AI\n\n${tree}" > README.md
