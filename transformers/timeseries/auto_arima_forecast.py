27e996ca5e8c4799cc0f6f76b9f66436e01272d6
goldentom42 <olivier.grellier@h2o.ai>
2019-06-21 08:09:18 +0200

    Add ARIMA documentation

--- transformers/timeseries/auto_arima_forecast.py
+++ transformers/timeseries/auto_arima_forecast.py
@@ -1,3 +1,8 @@
+"""Auto ARIMA transformer is a time series transformer that predicts target using ARIMA models"""
+
+# For more information about the python ARIMA package
+# please visit https://www.alkaline-ml.com/pmdarima/index.html
+
 import importlib
 
 from h2oaicore.transformer_utils import CustomTimeSeriesTransformer
@@ -17,18 +22,31 @@ class MyAutoArimaTransformer(CustomTimeSeriesTransformer):
         return dict(col_type="time_column", min_cols=1, max_cols=1, relative_importance=1)
 
     def fit(self, X: dt.Frame, y: np.array = None):
+        """
+        Fits ARIMA models (1 per time group) using historical target values contained in y
+        :param X: Datatable frame containing the features
+        :param y: numpy array containing the historical values of the target
+        :return: self
+        """
+        # Import the ARIMA python module
         pm = importlib.import_module('pmdarima')
+        # Init models
         self.models = {}
+        # Convert to pandas
         X = X.to_pandas()
         XX = X[self.tgc].copy()
         XX['y'] = np.array(y)
         self.nan_value = np.mean(y)
         self.ntrain = X.shape[0]
+
+        # Group the input by TGC (Time group column) excluding the time column itself
         tgc_wo_time = list(np.setdiff1d(self.tgc, self.time_column))
         if len(tgc_wo_time) > 0:
             XX_grp = XX.groupby(tgc_wo_time)
         else:
             XX_grp = [([None], XX)]
+
+        # Build 1 ARIMA model per time group columns
         for key, X in XX_grp:
             key = key if isinstance(key, list) else [key]
             grp_hash = '_'.join(map(str, key))
@@ -42,6 +60,13 @@ class MyAutoArimaTransformer(CustomTimeSeriesTransformer):
         return self
 
     def transform(self, X: dt.Frame):
+        """
+        Uses fitted models (1 per time group) to predict the target
+        If self.is_train exists, it means we are doing in-sample predictions
+        if it does not then we Arima is used to predict the future
+        :param X: Datatable Frame containing the features
+        :return: ARIMA predictions
+        """
         X = X.to_pandas()
         XX = X[self.tgc].copy()
         tgc_wo_time = list(np.setdiff1d(self.tgc, self.time_column))
@@ -73,12 +98,27 @@ class MyAutoArimaTransformer(CustomTimeSeriesTransformer):
         return XX
 
     def fit_transform(self, X: dt.Frame, y: np.array = None):
+        """
+        Fits the ARIMA models (1 per time group) and outputs the corresponding predictions
+        :param X: Datatable Frame
+        :param y: Target to be used to fit the ARIMA model and perdict in-sample
+        :return: in-sample ARIMA predictions
+        """
+        # Flag the fact we are doing in-sample predictions
         self.is_train = True
         ret = self.fit(X, y).transform(X)
         del self.is_train
         return ret
 
     def update_history(self, X: dt.Frame, y: np.array = None):
+        """
+        Update the model fit with additional observed endog/exog values.
+        Updating an ARIMA adds new observations to the model, updating the MLE of the parameters
+        accordingly by performing several new iterations (maxiter) from the existing model parameters.
+        :param X: Datatable Frame containing input features
+        :param y: Numpy array containing new observations to update the ARIMA model
+        :return:
+        """
         X = X.to_pandas()
         XX = X[self.tgc].copy
         XX['y'] = np.array(y)
