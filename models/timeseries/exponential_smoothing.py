52ca15e0aa213356d094af37bb5706b30f6b6bef
Arno Candel <arno.candel@gmail.com>
2019-06-22 12:01:19 -0700

    Cleanup spaces.

--- models/timeseries/exponential_smoothing.py
+++ models/timeseries/exponential_smoothing.py
@@ -1,4 +1,4 @@
-"""Linear Model on top of Exponential Weighted Moving Average Lags for Time-Series.
+"""Linear Model on top of Exponential Weighted Moving Average Lags for Time-Series. 
 Provide appropriate lags and past outcomes during batch scoring for best results."""
 from h2oaicore.models import BaseCustomModel, GLMModel
 import numpy as np
