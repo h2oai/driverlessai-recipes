e34461b624eb0cc87f6e932df77c3b4d8fe1e16b
Arno Candel <arno.candel@gmail.com>
2019-06-22 12:17:33 -0700

    Add flag for missing values.

---
+++ transformers/generic/missing_flag_transformer.py
@@ -0,0 +1,16 @@
+"""Returns 1 if a value is missing, or 0 otherwise"""
+from h2oaicore.transformer_utils import CustomTransformer
+import datatable as dt
+import numpy as np
+
+
+class FlagMissingTransformer(CustomTransformer):
+    @staticmethod
+    def get_default_properties():
+        return dict(col_type="all", min_cols=1, max_cols=1, relative_importance=1)
+
+    def fit_transform(self, X: dt.Frame, y: np.array = None):
+        return self.transform(X)
+
+    def transform(self, X: dt.Frame):
+        return X[:, dt.isna(dt.f[0])]
