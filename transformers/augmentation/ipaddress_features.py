c667a6af4c247d299abde1799ea81ad72a667a3c
Arno Candel <arno.candel@gmail.com>
2019-06-24 14:59:25 -0700

    Formatting

--- transformers/augmentation/ipaddress_features.py
+++ transformers/augmentation/ipaddress_features.py
@@ -5,6 +5,7 @@ import numpy as np
 from abc import ABC, abstractmethod
 import ipaddress
 
+
 class IPAddressBaseTransformer(ABC):
     @staticmethod
     def get_default_properties():
@@ -13,26 +14,26 @@ class IPAddressBaseTransformer(ABC):
     @abstractmethod
     def get_ip_property(self, value):
         raise NotImplementedError
-    
+
     def parse_ipaddress(self, value):
         try:
             result = ipaddress.ip_address(value)
         except ValueError:
             result = ipaddress.ip_network(value)
         return result
-        
+
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         return self.transform(X)
-        
+
     def transform(self, X: dt.Frame):
-        
+
         try:
-            return X[:, {"x": (dt.isna(dt.f[0])) & None | self.get_ip_property(self.parse_ipaddress(dt.f[0])) }]
+            return X[:, {"x": (dt.isna(dt.f[0])) & None | self.get_ip_property(self.parse_ipaddress(dt.f[0]))}]
             # return X.to_pandas().astype(str).iloc[:, 0].apply(lambda x: self.get_ip_property(self.parse_ipaddress(x)))
-            
+
         except ValueError:
             return np.zeros(X.shape[0])
-    
+
 
 class IPAddressAsIntegerTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
@@ -44,31 +45,38 @@ class IPAddressAsIntegerTransformer(IPAddressBaseTransformer, CustomTransformer)
             return int(value[0])
         else:
             raise ValueError
-    
+
+
 class IsIPAddressMulticastTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_multicast
+
 
 class IsIPAddressPrivateTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_private
 
+
 class IsIPAddressGlobalTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_global
-    
+
+
 class IsIPAddressUnspecifiedTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_unspecified
-    
+
+
 class IsIPAddressReservedTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_reserved
-    
+
+
 class IsIPAddressLoopbackTransformer(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_loopback
-    
+
+
 class IsIPAddressLinkLocal(IPAddressBaseTransformer, CustomTransformer):
     def get_ip_property(self, value):
         return value.is_link_local
