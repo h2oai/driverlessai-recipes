d33f238b34014bc20d7a7344ee88c3e372cbc31d
Arno Candel <arno.candel@gmail.com>
2019-06-25 16:58:04 -0700

    Fix new binary classification scorers.

--- scorers/classification/binary/false_positive_count.py
+++ scorers/classification/binary/false_positive_count.py
@@ -1,18 +1,17 @@
-"""Optimizes for specific Confusion Matrix Values: TP, TN, FP, or FN"""
+"""Optimizes for specific Confusion Matrix Values: FP"""
 import typing
 import numpy as np
 from h2oaicore.metrics import CustomScorer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 
-class CMFalseNegative(CustomScorer):
 
-    _description = "Reduce false negative count"
+class CMFalsePositive(CustomScorer):
+    _description = "Reduce false positive count"
     _binary = True
-    _multiclass = True
     _maximize = False
     _perfect_score = 0
-    _display_name = "False Negative Count"
+    _display_name = "FP"
     _threshold = 0.5
 
     def score(self,
@@ -20,21 +19,12 @@ class CMFalseNegative(CustomScorer):
               predicted: np.array,
               sample_weight: typing.Optional[np.array] = None,
               labels: typing.Optional[np.array] = None) -> float:
-
-        # label actual values
         lb = LabelEncoder()
         labels = lb.fit_transform(labels)
         actual = lb.transform(actual)
-
-        if len(labels) > 2:
-            predicted = np.argmax(predicted, axis=1)
-        else:
-            predicted = (predicted > self._threshold)
-
-        # use sklean to get values
+        predicted = (predicted > self._threshold)
         cm = confusion_matrix(actual, predicted, sample_weight=sample_weight, labels=labels)
         tn, fp, fn, tp = cm.ravel()
-
-        return fn
+        return fp
 
 
--- scorers/classification/binary/true_negative_count.py
+++ scorers/classification/binary/true_negative_count.py
@@ -1,18 +1,17 @@
-"""Optimizes for specific Confusion Matrix Values: TP, TN, FP, or FN"""
+"""Optimizes for specific Confusion Matrix Values: TN"""
 import typing
 import numpy as np
 from h2oaicore.metrics import CustomScorer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 
-class CMTruePositive(CustomScorer):
 
-    _description = "Increase true positive count"
+class CMTrueNegative(CustomScorer):
+    _description = "Increase true negative count"
     _binary = True
-    _multiclass = True
     _maximize = True
-    _perfect_score = 10000000000
-    _display_name = "True Positive Count"
+    _perfect_score = 1e20
+    _display_name = "TN"
     _threshold = 0.5
 
     def score(self,
@@ -20,21 +19,12 @@ class CMTruePositive(CustomScorer):
               predicted: np.array,
               sample_weight: typing.Optional[np.array] = None,
               labels: typing.Optional[np.array] = None) -> float:
-
-        # label actual values
         lb = LabelEncoder()
         labels = lb.fit_transform(labels)
         actual = lb.transform(actual)
-
-        if len(labels) > 2:
-            predicted = np.argmax(predicted, axis=1)
-        else:
-            predicted = (predicted > self._threshold)
-
-        # use sklean to get values
+        predicted = (predicted > self._threshold)
         cm = confusion_matrix(actual, predicted, sample_weight=sample_weight, labels=labels)
         tn, fp, fn, tp = cm.ravel()
-
-        return tp
+        return tn
 
 
