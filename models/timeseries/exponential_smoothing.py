85310905fdf0022d9d452405cab7dc6c3cba897c
Jonathan C. McKinney <pseudotensor@gmail.com>
2019-07-19 15:31:17 -0700

    identify TS-only type model so not used if not TS

--- models/timeseries/exponential_smoothing.py
+++ models/timeseries/exponential_smoothing.py
@@ -8,6 +8,15 @@ class ExponentialSmoothingModel(BaseCustomModel, GLMModel):
     _regression = True
     _binary = False
     _multiclass = False
+    _time_series_only = True
     _display_name = "EWMA_GLM"
     _description = "GLM with EWMA Lags"
     _included_transformers = ["EwmaLagsTransformer"]
+
+    @staticmethod
+    def can_use(accuracy, interpretability, train_shape=None, test_shape=None, valid_shape=None, n_gpus=0):
+        return True  # i.e. ignore GLM's restrictions on interpretability and accuracy
+
+    @staticmethod
+    def enabled_setting():
+        return "on"  # i.e. ignore GLM's default choice of auto that would disable this model if too many classes
