47abf38feab2de32649194d511a11ee1b3c54934
Michelle Tanco <michelle.tanco@h2o.ai>
2019-08-06 22:20:23 -0700

    adding transformers for numeric interactions with 3+ columns

---
+++ transformers/numeric/product.py
@@ -0,0 +1,29 @@
+"""Products together 3 or more numeric features"""
+from h2oaicore.transformer_utils import CustomTransformer
+import datatable as dt
+import numpy as np
+
+
+class ProductTransformer(CustomTransformer):
+
+    @staticmethod
+    def is_enabled():
+        return True
+
+    @staticmethod
+    def do_acceptance_test():
+        return True
+
+    @staticmethod
+    def get_default_properties():
+        return dict(col_type="numeric", min_cols=3, max_cols="all", relative_importance=1)
+
+    def fit_transform(self, X: dt.Frame, y: np.array = None):
+        return self.transform(X)
+
+    def transform(self, X: dt.Frame):
+
+        df = X.to_pandas()
+        df['_value_to_return'] = df.product(axis=1, skipna=True)
+
+        return df['_value_to_return']
