82c383dec73aa6e09d0a71aa812aad8778808506
Bojan Tunguz <tunguz@gmail.com>
2019-08-09 12:54:46 -0700

    Update square_root_transformer.py

--- transformers/numeric/square_root_transformer.py
+++ transformers/numeric/square_root_transformer.py
@@ -4,7 +4,7 @@ import datatable as dt
 import numpy as np
 
 
-class MyLogTransformer(CustomTransformer):
+class SquareRootTransformer(CustomTransformer):
     @staticmethod
     def get_default_properties():
         return dict(col_type="numeric", min_cols=1, max_cols=3, relative_importance=1)
@@ -14,19 +14,3 @@ class MyLogTransformer(CustomTransformer):
 
     def transform(self, X: dt.Frame):
         return X[:, [(dt.f[i]/dt.abs(dt.f[i]))*dt.exp(0.5*dt.log(dt.abs(dt.f[i]))) for i in range(X.ncols)]]
-
-    # optional
-    _mojo = True
-    from h2oaicore.mojo import MojoWriter, MojoFrame
-
-    def to_mojo(self, mojo: MojoWriter, iframe: MojoFrame):
-        from h2oaicore.mojo import MojoColumn, MojoFrame
-        from h2oaicore.mojo_transformers import MjT_Log
-        xnew = iframe[self.input_feature_names]
-        oframe = MojoFrame()
-        for col in xnew:
-            ocol = MojoColumn(name=col.name, dtype=np.float64)
-            ocol_frame = MojoFrame(columns=[ocol])
-            mojo += MjT_Log(iframe=MojoFrame(columns=[col]), oframe=ocol_frame)
-            oframe += ocol
-        return oframe
