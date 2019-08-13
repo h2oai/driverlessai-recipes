9869457fa7d7ea003df43b1356fb76a2f6270974
Arno Candel <arno.candel@gmail.com>
2019-08-13 11:57:27 -0700

    Cleanup

--- transformers/numeric/count_negative_values_transformer.py
+++ transformers/numeric/count_negative_values_transformer.py
@@ -13,11 +13,4 @@ class CountNegativePerRowTransformer(CustomTransformer):
         return self.transform(X)
 
     def transform(self, X: dt.Frame):
-        if X.ncols == 0:
-            return np.zeros(X.nrows)
         return X[:, dt.sum([(dt.f[x] < 0) for x in range(X.ncols)])]
-
-
-class CountNegativeNumericsPerRowTransformer(CountNegativePerRowTransformer):
-    def transform(self, X: dt.Frame):
-        return super().transform(X[:, [int, float]])
