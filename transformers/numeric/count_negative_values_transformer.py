f5bd346e274b4b6f032ef137dc0bef1d08fa910c
Arno Candel <arno.candel@gmail.com>
2019-08-12 18:15:14 -0700

    Fix positive/negative values per row transformers.

--- transformers/numeric/count_negative_values_transformer.py
+++ transformers/numeric/count_negative_values_transformer.py
@@ -1,13 +1,13 @@
-"""Count of positive values per row"""
+"""Count of negative values per row"""
 from h2oaicore.transformer_utils import CustomTransformer
 import datatable as dt
 import numpy as np
 
 
-class CountPositivePerRowTransformer(CustomTransformer):
+class CountNegativePerRowTransformer(CustomTransformer):
     @staticmethod
     def get_default_properties():
-        return dict(col_type="all", min_cols="all", max_cols="all", relative_importance=1)
+        return dict(col_type="numeric", min_cols="all", max_cols="all", relative_importance=1)
 
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         return self.transform(X)
@@ -15,10 +15,9 @@ class CountPositivePerRowTransformer(CustomTransformer):
     def transform(self, X: dt.Frame):
         if X.ncols == 0:
             return np.zeros(X.nrows)
-        return X[:, dt.sum([(dt.f[x] > 0) for x in range(X.ncols)])]
+        return X[:, dt.sum([(dt.f[x] < 0) for x in range(X.ncols)])]
 
 
-class CountPositiveNumericsPerRowTransformer(CountPositivePerRowTransformer):
+class CountNegativeNumericsPerRowTransformer(CountNegativePerRowTransformer):
     def transform(self, X: dt.Frame):
         return super().transform(X[:, [int, float]])
-
