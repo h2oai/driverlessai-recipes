d85b88dec94044bb50027b5eee5af152e13f19d5
goldentom42 <olivier.grellier@h2o.ai>
2019-09-07 15:01:03 +0200

    Add hourly aggrgation for bigger uplift

--- recipes/ieee_fraud_2019.py
+++ recipes/ieee_fraud_2019.py
@@ -1,12 +1,10 @@
 import datatable as dt
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-
-from h2oaicore.models import CustomModel, BaseCustomModel, LightGBMModel
 from h2oaicore.transformer_utils import CustomTransformer
-from h2oaicore.systemutils import config, physical_cores_count
 import datetime
+from h2oaicore.systemutils import make_experiment_logger, loggerinfo, loggerwarning, printflush_debug, printflush_info
+
+
 
 
 def get_datetime_from_transactionDT(X: dt.Frame):
@@ -62,9 +60,13 @@ class MyIEEEDateTimeTransformer(CustomTransformer):
 
 class MyIEEEGroupBysTransformers(CustomTransformer):
 
-    def __init__(self, group_col, **kwargs):
+    def __init__(self, group_col, group_type, **kwargs):
         super().__init__(**kwargs)
         self.group_col = group_col
+        self.group_type = group_type
+        print('=' * 50)
+        print("MyIEEEGroupBysTransformers will use {} and {} for groupby".format(self.group_col, self.group_type))
+        print('=' * 50)
 
     @property
     def display_name(self):
@@ -76,10 +78,9 @@ class MyIEEEGroupBysTransformers(CustomTransformer):
 
     @staticmethod
     def get_parameter_choices():
-        return {"group_col": ['card1', 'card2', 'card3', 'card4']}
+        return {"group_col": ['card1', 'card2', 'card3', 'card4'], 'group_type': ['day', 'hour']}
 
     def fit_transform(self, X: dt.Frame, y: np.array = None):
-
         return self.transform(X)
 
     def transform(self, X: dt.Frame):
@@ -87,24 +88,63 @@ class MyIEEEGroupBysTransformers(CustomTransformer):
 
         if (self.group_col in X.names) & (ieee_datetime is not None):
 
-            X = dt.Frame(X[:, self.group_col])
-            X[:, 'date'] = dt.Frame(ieee_datetime.dt.strftime('%Y%m%d').values)
+            if self.group_type == 'day':
+                X = dt.Frame(X[:, self.group_col])
+                X[:, 'date'] = dt.Frame(ieee_datetime.dt.strftime('%Y%m%d').values)
 
-            # Compute daily counts
-            daily_cnt = X[:, {"daily_cnt": dt.count()}, dt.by("date")]
-            daily_cnt.key = ["date"]
-            X = X[:, :, dt.join(daily_cnt)]
+                # Compute daily counts
+                daily_cnt = X[:, {"daily_cnt": dt.count()}, dt.by("date")]
+                daily_cnt.key = ["date"]
+                X = X[:, :, dt.join(daily_cnt)]
 
-            # Compute card count
-            col_cnt = X[:, {"col_cnt": dt.count()}, dt.by(*["date", self.group_col])]
-            col_cnt.key = ["date", self.group_col]
-            X = X[:, :, dt.join(col_cnt)]
+                # Compute card count
+                col_cnt = X[:, {"col_cnt": dt.count()}, dt.by(*["date", self.group_col])]
+                col_cnt.key = ["date", self.group_col]
+                X = X[:, :, dt.join(col_cnt)]
 
-            self._output_feature_names = ["IEEEGroupBys_" + self.group_col]
-            self._feature_desc = ["IEEEGroupBys_" + self.group_col]
+                self._output_feature_names = ["IEEEGroupBys_" + self.group_col]
+                self._feature_desc = ["IEEEGroupBys_" + self.group_col]
 
-            return X[:, dt.f["col_cnt"] / dt.f["daily_cnt"]]
+                print('=' * 50)
+                print("MyIEEEGroupBysTransformers name  {} {}".format(
+                    self._output_feature_names, self._feature_desc
+                )
+                )
+
+                return X[:, dt.f["col_cnt"] / dt.f["daily_cnt"]]
+
+            elif self.group_type == 'hour':
+
+                X = dt.Frame(X[:, self.group_col])
+                X[:, 'date'] = dt.Frame(ieee_datetime.dt.strftime('%Y%m%d_%H').values)
+
+                # Compute daily counts
+                hourly_cnt = X[:, {"hourly_cnt": dt.count()}, dt.by("date")]
+                hourly_cnt.key = ["date"]
+                X = X[:, :, dt.join(hourly_cnt)]
+
+                # Compute card count
+                col_cnt = X[:, {"col_cnt": dt.count()}, dt.by(*["date", self.group_col])]
+                col_cnt.key = ["date", self.group_col]
+                X = X[:, :, dt.join(col_cnt)]
+
+                self._output_feature_names = ["IEEEGroupBys_{}_{}".format(self.group_col, self.group_type)]
+                self._feature_desc = ["IEEEGroupBys_{}_{}".format(self.group_col, self.group_type)]
+
+                print('=' * 50)
+                print("MyIEEEGroupBysTransformers name  {} {}".format(
+                    self._output_feature_names, self._feature_desc
+                )
+                )
+
+                return X[:, dt.f["col_cnt"] / dt.f["hourly_cnt"]]
+
 
         else:
-
+            print('='*50)
+            print("MyIEEEGroupBysTransformers ERROR  {} {}".format(
+                    (self.group_col in X.names), (ieee_datetime is not None)
+                )
+            )
+            print('=' * 50)
             return np.zeros(X.shape[0])
