63d904c2d699b37d4b2fef3266d2adf8429a90ae
Marios Michailidis <kazanovassoftware@gmail.com>
2019-09-09 14:57:01 +0100

    WAPE transformer
    WAPE (Weighted Absolute Percent Error.) Transformer - an alternative of MAPE

---
+++ scorers/regression/WAPE_scorer.py
@@ -0,0 +1,19 @@
+import typing
+import numpy as np
+from h2oaicore.metrics import CustomScorer
+from sklearn.metrics import median_absolute_error
+
+class MyWAPE(CustomScorer):
+    _description = "Weighted Absolute Percent Error. "
+    _regression = True
+    _maximize = False
+    _perfect_score = 0
+    _supports_sample_weight=False    
+    _display_name = "WAPE"
+
+    def score(self,
+              actual: np.array,
+              predicted: np.array,
+              sample_weight: typing.Optional[np.array] = None,
+              labels: typing.Optional[np.array] = None) -> float:
+        return (abs(actual - predicted).sum()/actual.sum())
