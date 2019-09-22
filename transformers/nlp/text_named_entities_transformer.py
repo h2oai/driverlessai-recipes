6b0f60b9b636c3553835d5dc86149db9af49647f
Lena <lena.jer@gmail.com>
2019-09-22 19:26:31 +0200

    merging per entity type transformers to one

--- transformers/nlp/text_named_entities_transformer.py
+++ transformers/nlp/text_named_entities_transformer.py
@@ -6,10 +6,9 @@ import numpy as np
 from h2oaicore.transformer_utils import CustomTransformer
 
 
-class NamedEntityTransformer:
+class TextNamedEntityTransformer(CustomTransformer):
     """Transformer to extract the count of Named Entities"""
-    _method = NotImplemented
-    _modules_needed_by_name = ["spacy==2.1.8"]
+    _modules_needed_by_name = ["spacy>=2.1.8"]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -21,56 +20,26 @@ class NamedEntityTransformer:
             from spacy.cli import download
             download('en_core_web_sm')
             self.nlp = spacy.load('en_core_web_sm')
+        self.ne_types = {"PERSON", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "DATE"}
 
     @staticmethod
     def get_default_properties():
         return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
 
     def get_ne_count(self, text):
-        ne_type = self.__class__._method
         entities = self.nlp(text).ents
         if entities:
-            return len([entity for entity in entities if entity.label_ == ne_type])
+            return [len([entity for entity in entities if entity.label_ == ne_type]) for ne_type in self.ne_types]
         else:
-            return 0
+            return [0]*len(self.ne_types)
 
     def fit_transform(self, X: dt.Frame, y: np.array = None):
         return self.transform(X)
 
     def transform(self, X: dt.Frame):
-        return X.to_pandas().astype(str).fillna("NA").iloc[:, 0].apply(lambda x: self.get_ne_count(x))
+        orig_col_name = X.names[0]
+        X = dt.Frame(X).to_pandas().astype(str).fillna("NA")
+        new_X = X.apply(lambda x: self.get_ne_count(x[orig_col_name]), axis=1, result_type='expand')
+        new_X.columns = [f'{orig_col_name}_Count_{ne_type}' for ne_type in self.ne_types]
+        return new_X
 
-
-class PersonCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of Persons in the text column"""
-    _method = "PERSON"
-
-
-class OrgCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of organizations in the text column"""
-    _method = "ORG"
-
-
-class GeoCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of countries, cities, states in the text column"""
-    _method = "GPE"
-
-
-class LocCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of non-GPE locations in the text column"""
-    _method = "LOC"
-
-
-class ProductCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of products (objects, vehicles, foods, etc.) in the text column"""
-    _method = "PRODUCT"
-
-
-class EventCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of events (named hurricanes, battles, wars, sports events, etc.) in the text column"""
-    _method = "EVENT"
-
-
-class DateCountTransformer(NamedEntityTransformer, CustomTransformer):
-    """Get the count of dates and periods in the text column"""
-    _method = "DATE"
