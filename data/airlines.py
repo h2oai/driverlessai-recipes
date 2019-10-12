3f2c7a3d89f833d4806720fce10da3866cf58c4a
Jonathan C. McKinney <pseudotensor@gmail.com>
2019-10-12 02:46:26 -0700

    Further simplify airlines data recipe

--- data/airlines.py
+++ data/airlines.py
@@ -9,18 +9,14 @@ class AirlinesData(CustomData):
 
 def _create_data(input_file=""):
     import os
-    from h2oaicore.systemutils_more import extract, download
+    from h2oaicore.systemutils_more import download
     from h2oaicore.systemutils import config
-    import shutil
     import bz2
 
     def extract_bz2(file, output_file):
-        zipfile = bz2.BZ2File(file) # open the file
-        data = zipfile.read() # get the decompressed data
-        open(output_file, 'wb').write(data) # write a uncompressed file
-
-    is_installed_path = os.path.join(config.data_directory, config.contrib_env_relative_directory, "airlines")
-    is_installed_file = os.path.join(is_installed_path, "airlines_is_installed")
+        zipfile = bz2.BZ2File(file)
+        data = zipfile.read()
+        open(output_file, 'wb').write(data)
 
     temp_path = os.path.join(config.data_directory, config.contrib_relative_directory, "airlines")
     os.makedirs(temp_path, exist_ok=True)
@@ -28,12 +24,7 @@ def _create_data(input_file=""):
     link = "http://stat-computing.org/dataexpo/2009/1987.csv.bz2"
     file = download(link, dest_path=temp_path)
     output_file = file.replace(".bz2", "")
-
-    if not os.path.isfile(is_installed_file):
-        extract_bz2(file, output_file)
-
-        os.makedirs(is_installed_path, exist_ok=True)
-        with open(is_installed_file, "wt") as f:
-            f.write("DONE")
+    print("%s %s" % (file, output_file))
+    extract_bz2(file, output_file)
 
     return output_file
