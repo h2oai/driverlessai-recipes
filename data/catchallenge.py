3af55a69f8b28602e77598f9900b9dcedc9266e8
Jonathan C. McKinney <pseudotensor@gmail.com>
2019-10-30 13:06:55 -0700

    typo and clean-up

--- data/catchallenge.py
+++ data/catchallenge.py
@@ -1,27 +1,30 @@
-"""Create airlines dataset"""
+"""Create cat challenge dataset"""
 
 from typing import Union, List
 from h2oaicore.data import CustomData
 import datatable as dt
 import numpy as np
 import pandas as pd
+import os
 
 
 class CatChallenge(CustomData):
     @staticmethod
     def create_data(X: dt.Frame = None):
 
-        # to be usd with models.algorithms.logistic_regression.py with
+        # to be used with models.algorithms.logistic_regression.py with
         # _kaggle = True
         # _kaggle_features = True
         # _kaggle_mode = True
-        train = dt.fread("/home/jon/kaggle/cat/inputs/train.csv.zip")
+        path = "/home/jon/kaggle/cat/inputs/"
+
+        train = dt.fread(os.path.join(path, "train.csv.zip"))
         train_orig = dt.Frame(train)
         train['sample_weight'] = dt.Frame(np.array([1.0] * train.shape[0]))
-        test = dt.fread("/home/jon/kaggle/cat/inputs/test.csv.zip")
+        test = dt.fread(os.path.join(path, "test.csv.zip"))
         test_orig = dt.Frame(test)
         test['sample_weight'] = dt.Frame(np.array([0.0] * test.shape[0]))
-        test['target'] = dt.Frame(np.array([0.0] * test.shape[0]))
+        test['target'] = dt.Frame(np.array([0] * test.shape[0], dtype=int))
         final = dt.rbind([train, test])
 
         return {'catmerged': final, 'cattrain': train_orig, 'cattest': test_orig}
