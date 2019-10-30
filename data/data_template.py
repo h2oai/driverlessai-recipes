dac4934937ddbaa8fa5d787620b4ad3ad1475a36
Arno Candel <arno.candel@gmail.com>
2019-10-30 12:59:14 -0700

    Update custom data template.

--- data/data_template.py
+++ data/data_template.py
@@ -6,18 +6,30 @@ import pandas as pd
 from typing import Union, List
 from h2oaicore.data import BaseData
 
+_global_modules_needed_by_name = []  # Optional global package requirements, for multiple custom recipes in a file
+
 
 class CustomData(BaseData):
     """Base class for a custom data creation recipe that can be specified externally to Driverless AI.
+    To use as recipe, in the class replace CustomData with your class name and replace BaseData with CustomData
 
     Note: Experimental API, will most likely change in future versions.
     """
 
+    """Specify the python package dependencies (will be installed via pip install mypackage==1.3.37)"""
+    _modules_needed_by_name = []  # List[str], e.g., ["mypackage==1.3.37"]
+
     @staticmethod
-    def create_data(X: dt.Frame = None) -> Union[str, List[str],
-                                                 dt.Frame, List[dt.Frame],
-                                                 np.ndarray, List[np.ndarray],
-                                                 pd.DataFrame, List[pd.DataFrame]]:
+    def create_data(X: dt.Frame = None) -> Union[
+        str, List[str],
+        dt.Frame, List[dt.Frame],
+        np.ndarray, List[np.ndarray],
+        pd.DataFrame, List[pd.DataFrame],
+        Dict[str, str],  # {data set names : paths}
+        Dict[str, dt.Frame],  # {data set names : dt frames}
+        Dict[str, np.ndarray],  # {data set names : np arrays}
+        Dict[str, pd.DataFrame],  # {data set names : pd frames}
+    ]:
         """
         Create one or multiple datasets to be imported into Driverless AI
 
