47092681f48cf5a2d09517139d863281da3c0646
Qiang Kou (KK) <qkou@qkou.info>
2019-11-21 15:42:23 -0800

    add group id into model and transformer template

--- transformers/transformer_template.py
+++ transformers/transformer_template.py
@@ -240,7 +240,7 @@ class CustomTransformer(DataTableTransformer):
         """
         raise NotImplementedError("Please implement the transform method.")
 
-    def to_mojo(self, mojo: MojoWriter, iframe: MojoFrame):  # -> MojoFrame:
+    def to_mojo(self, mojo: MojoWriter, iframe: MojoFrame, group_uuid=None, group_name=None):  # -> MojoFrame:
         """
         Optional method to implement MOJO writing - expert mode
         """
