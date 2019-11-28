5896a0020510382c4e4ae733352401fa937dac59
vopani <rohanrao88@gmail.com>
2019-11-28 15:23:39 +0530

    made generic map@k

--- scorers/classification/multiclass/map@k.py
+++ scorers/classification/multiclass/map@k.py
@@ -1,33 +1,36 @@
 """
-Mean Average Precision @ 5 (MAP@5)
+Mean Average Precision @ k (MAP@k)
 https://www.kaggle.com/c/expedia-hotel-recommendations/overview/evaluation
 """
 import typing
 import numpy as np
 import pandas as pd
+
 from h2oaicore.metrics import CustomScorer
+from h2oaicore.systemutils import config
 
 
-class MAPat5(CustomScorer):
-    _description = "Mean Average Precision @ 5 (MAP@5)"
+class MAPatk(CustomScorer):
+    _description = "Mean Average Precision @ k (MAP@k)"
     _multiclass = True
     _maximize = True
     _perfect_score = 1
-    _display_name = "MAP@5"
+    _display_name = "MAP@k"
 
     def score(self, actual: np.array, predicted: np.array, sample_weight: typing.Optional[np.array] = None,
               labels: typing.Optional[np.array] = None) -> float:
-        predicted5 = [preds.argsort()[-5:][::-1] for preds in predicted]
-        predicted_labels = [[labels[x] for x in preds] for preds in predicted5]
+        k = config.recipe_dict['k_for_map'] if 'k_for_map' in config.recipe_dict else 5
+        predictedk = [preds.argsort()[-k:][::-1] for preds in predicted]
+        predicted_labels = [[labels[x] for x in preds] for preds in predictedk]
         df = pd.DataFrame.from_records(predicted_labels)
-        map5 = map5eval(df, actual, len(labels))
-        return map5
+        mapk = mapkeval(df, actual, len(labels), k)
+        return mapk
 
 
-def map5eval(predicted, actual, n_classes):
+def mapkeval(predicted, actual, n_classes, k):
     metric = 0.
-    if n_classes > 4:
-        for i in range(5):
+    if n_classes > k-1:
+        for i in range(k):
             pred = pd.Series.tolist(predicted.iloc[:, i])
             metric += np.sum(actual == pred) / (i + 1)
         metric /= actual.shape[0]
