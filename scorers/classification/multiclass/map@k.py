7a4ad4036fdd89bc8360c5127632ebda9b315f5c
Lena <lena.jer@gmail.com>
2019-12-04 15:06:28 +0100

    Number of classes to be used as default K if smaller than default 10/ user set K

--- scorers/classification/multiclass/map@k.py
+++ scorers/classification/multiclass/map@k.py
@@ -4,6 +4,7 @@ Mean Average Precision @ k (MAP@k)
 
 """
 Add value of k in recipe_dict in config or scorer will use default value of 10
+If the number of classes is less than the chosen value, use number of classes instead.
 
 Sample Datasets
 # Expedia - https://www.kaggle.com/c/expedia-hotel-recommendations/overview/evaluation
@@ -24,24 +25,24 @@ class MAPatk(CustomScorer):
     _maximize = True
     _perfect_score = 1
     _display_name = "MAP@k"
+    _supports_sample_weight = False
 
     def score(self, actual: np.array, predicted: np.array, sample_weight: typing.Optional[np.array] = None,
               labels: typing.Optional[np.array] = None) -> float:
-        k = config.recipe_dict['k_for_map'] if 'k_for_map' in config.recipe_dict else 10
+        num_classes = len(labels)
+        default_k = 10 if num_classes > 10 else num_classes
+        k = config.recipe_dict['k_for_map'] if 'k_for_map' in config.recipe_dict else default_k
         predictedk = [preds.argsort()[-k:][::-1] for preds in predicted]
         predicted_labels = [[labels[x] for x in preds] for preds in predictedk]
         df = pd.DataFrame.from_records(predicted_labels)
-        mapk = mapkeval(df, actual, len(labels), k)
+        mapk = mapkeval(df, actual, num_classes, k)
         return mapk
 
 
 def mapkeval(predicted, actual, n_classes, k):
     metric = 0.
-    if n_classes >= k:
-        for i in range(k):
-            pred = pd.Series.tolist(predicted.iloc[:, i])
-            metric += np.sum(actual == pred) / (i + 1)
-        metric /= actual.shape[0]
-    else:
-        return 0.
+    for i in range(k):
+        pred = pd.Series.tolist(predicted.iloc[:, i])
+        metric += np.sum(actual == pred) / (i + 1)
+    metric /= actual.shape[0]
     return metric
