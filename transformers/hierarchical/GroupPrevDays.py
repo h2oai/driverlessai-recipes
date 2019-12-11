7e2e26159e064e722b38a69b8bd6a7d6a0d9bd55
Michelle Tanco <michelle.tanco@h2o.ai>
2019-12-10 16:14:33 -0800

    improve comments and extensibility

--- transformers/hierarchical/GroupPrevDays.py
+++ transformers/hierarchical/GroupPrevDays.py
@@ -1,37 +1,39 @@
-# Author: Michelle Tanco - michelle.tanco@h2o.ai
-# Last Updated: December 4th, 2019
-# Purpose:  For non-time series experiments that have a time element
-#           Uses hard-coded group(s), time, and target values
-#           Counts number and percent of positive events in the previous days(s) by group
-#           If the previous row does not exist, that is the group is new, return the mode target event
+# Purpose: For transactional grouped data, calculate historical percentage of the target value
+#
+# Expected ML Problem: Transactional binary classification for many groups
+# Example Use Cases: part failure, credit card fraud
+# Overview: Takes hardcoded groups and time
+#
 # DAI Version: 1.8.0
+# Important Note: Models with this transformer should be retrained frequently
 
 
 from h2oaicore.transformer_utils import CustomTransformer
-# from h2oaicore.systemutils import make_experiment_logger, loggerinfo, loggerwarning
 import datatable as dt
 import numpy as np
 import pandas as pd
 from datetime import timedelta
 
-# Currently only for binary classification use cases
 class PCTDaysTransformer(CustomTransformer):
+    # only for binary classification
     _regression = False
     _binary = True
     _multiclass = False
+
     _numeric_output = True
     _is_reproducible = True
     _included_model_classes = None
     _excluded_model_classes = None
+
+    # give easy-to-understand names in the UI
     _allow_transform_to_modify_output_feature_names = True
 
-
-    # Make usable by default
+    # Allow transformer to be used by DAI
     @staticmethod
     def is_enabled():
         return True
 
-    # Cannot use DAI testing as we have hard coded column names
+    # Cannot use DAI code testing as we have hard coded column names
     @staticmethod
     def do_acceptance_test():
         return False
@@ -42,14 +44,13 @@ class PCTDaysTransformer(CustomTransformer):
         return dict(col_type="all", min_cols="all", max_cols="all", relative_importance=1)
 
     # Set the hard coded values about our data set
-    # TODO: rewrite this as config arguments so code does not need to be hard coded
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.col_date = "event_ts"
         self.col_group = ["customer_id"]
         self.target_positive = "Yes"
 
-        # the number of ROWS we want to look back
+        # the number of DAYS or Rows we want to look back
         self.timespans = [30, 60, 90]
 
     # This function runs during training
@@ -60,11 +61,11 @@ class PCTDaysTransformer(CustomTransformer):
         self._output_feature_names = []
         self._feature_desc = []
 
-        # group by and time
+        # list of group by and time columns
         cols_agg = self.col_group.copy()
         cols_agg.append(self.col_date)
 
-        # group by, time, and y
+        # list of group by, time, and y columns
         cols_agg_y = cols_agg.copy()
         cols_agg_y.append('col_target_bin')
 
@@ -78,20 +79,19 @@ class PCTDaysTransformer(CustomTransformer):
         # Make sure time is time
         X[self.col_date] = pd.to_datetime(X[self.col_date])
 
-        # Make sure we don't use target values at current row
         # We need to sort by date then groupby customer_id and finally shift 1 event
         X = X.sort_values(self.col_date)
         X['col_target_bin'] = X.groupby(self.col_group)['col_target_bin'].shift(1).fillna(0)
         # Go back to original index
         X.sort_index(inplace=True)
 
-        # Create a feature for each of the requested days
+        # Create a feature for each of the requested days / rows
         # Initialize the feature result with index set, this way features are added in correct row order even
         # if feature comes out with an unordered index
         feats_df = pd.DataFrame(index=X.index)
         for t in self.timespans:
             # Create feature name
-            t_days = str(t) + "d"
+            t_days = str(t) + "d" # use t instead of t_days if you want to aggregate by rows - other units of time can be used as well
             # Create the groups
             groups = X[cols_agg_y].groupby(self.col_group)
             # Run through groups
@@ -123,16 +123,13 @@ class PCTDaysTransformer(CustomTransformer):
 
         self.lookup_df = X.loc[time_filter].sort_values(self.col_date)
 
-        # :/ return the features
+        # return the features
         return feats_df
 
     # This function runs during scoring - it will look to what happened in the training data, so may get stale fast
     # If we see a new customer we return 0 for the features, as there are no key events in their past
     # It is also used to transform the validation set,
     # which means validation set must be after (time wise) the training set
-    # This function runs during scoring - it will look to what happened in the training data, so may get stale fast
-    # If we see a new customer we return 0 for the features, as there are no key events in their past
-    # It will also be used on the validation set during training
     def transform(self, X: dt.Frame):
 
         features = []
@@ -167,8 +164,11 @@ class PCTDaysTransformer(CustomTransformer):
             for _key, _df in groups:
                 # Sort the group by date, this is required for the rolling per time interval to work
                 _df = _df.sort_values(self.col_date)
-                # Apply rolling window
+                # Apply rolling window for last t Days
                 res = _df.set_index(self.col_date)['col_target_bin'].fillna(0).rolling(t_days, min_periods=1).mean()
+                # Apply rolling window for last t events
+                # res = _df.set_index(self.col_date)['col_target_bin'].fillna(0).rolling(t, min_periods=1).mean()
+
                 # It is not an apply and index may not be the original one
                 # Therefore make sure the index is the original one
                 res.index = _df.index
