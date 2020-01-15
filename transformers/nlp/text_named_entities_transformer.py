8a5515672e912842d9a7873502b2091bf3936933
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-01-14 18:34:33 -0800

    Avoid assigning module to self to avoid _pickle.PicklingError

--- transformers/nlp/text_named_entities_transformer.py
+++ transformers/nlp/text_named_entities_transformer.py
@@ -2,10 +2,7 @@
 
 import datatable as dt
 import numpy as np
-import os
-
 from h2oaicore.transformer_utils import CustomTransformer
-from h2oaicore.utils import ContribLoader
 
 
 class TextNamedEntityTransformer(CustomTransformer):
@@ -14,17 +11,14 @@ class TextNamedEntityTransformer(CustomTransformer):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        import spacy
-        import en_core_web_sm
-        self.nlp = en_core_web_sm.load()
         self.ne_types = {"PERSON", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "DATE"}
 
     @staticmethod
     def get_default_properties():
         return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
 
-    def get_ne_count(self, text):
-        entities = self.nlp(text).ents
+    def get_ne_count(self, text, nlp):
+        entities = nlp(text).ents
         if entities:
             return [len([entity for entity in entities if entity.label_ == ne_type]) for ne_type in self.ne_types]
         else:
@@ -34,9 +28,13 @@ class TextNamedEntityTransformer(CustomTransformer):
         return self.transform(X)
 
     def transform(self, X: dt.Frame):
+        import spacy
+        import en_core_web_sm
+        nlp = en_core_web_sm.load()
+
         orig_col_name = X.names[0]
         X = dt.Frame(X).to_pandas().astype(str).fillna("NA")
-        new_X = X.apply(lambda x: self.get_ne_count(x[orig_col_name]), axis=1, result_type='expand')
+        new_X = X.apply(lambda x: self.get_ne_count(x[orig_col_name], nlp), axis=1, result_type='expand')
         new_X.columns = [f'{orig_col_name}_Count_{ne_type}' for ne_type in self.ne_types]
         return new_X
 
