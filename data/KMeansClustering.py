affbcc3ef5dffdb887295af2466626ae7b0f3258
shivambansal <shivam5992@gmail.com>
2020-01-20 14:35:40 +0800

    updated Kmeans data recipe for 1.8.2

--- data/KMeansClustering.py
+++ data/KMeansClustering.py
@@ -1,20 +1,16 @@
 """ 
 Data Recipe to perform KMeans Clustering on a dataset. 
 
+__version__ = 0.1
+
 authored by @goldentom42 (Olivier Grellier)
 modified by @shivam5992  (Shivam Bansal)
 
-__version__ = 0.1
 
 Note:
 
-1. This recipe can be applied on any dataset, applies Kmeans clustering and creates 
-   and extra column in the dataset which is the cluster_id of each row in the dataset.
-
-2. Currently it only uses numeric columns and ignores the categorical/text columns.
-
-3. Users can define user inputs (global variables) for this recipe. They are following: 
-   number of clusters, columns to ignore, columns to include. (see section : Optional User Inputs)
+Users can define (optional) user inputs: number of clusters, columns to ignore, columns to include. 
+(see section : Optional User Inputs)
 
 """
 
@@ -37,21 +33,20 @@ from sklearn.preprocessing import LabelEncoder
     "" Optional User Inputs "" 
 
     | variable        | type | default | description                                             |
-    | ignore_columns  | list | None    | Columns to ignore in the model, example : ID columns    |
-    |                                    if not specified anything, all columns are considered   | 
-    | include_columns | list | None    | Columns to specifically include in the clustering model | 
-    |                                    if not specified anything, all columns are considered   |
+    | ignore_columns  | list | []      | Columns to ignore in the model, example : ID columns    |
+    |                                    if not specified, all columns are considered            | 
+    | include_columns | list | []      | Columns to specifically include in the clustering model | 
+    |                                    if not specified, all columns are considered            |
     | num_clusters    | int  | 0       | Number of clusters to generate in the output            | 
-                                         if not specified, the optimal number is obtained        | 
+                                         if not specified, optimal number is obtained            | 
 
 """
-ignore_columns = None  # Example ['ID', 'default payment next month']
-include_columns = None # Example ["Age", "Gender"]
-num_clusters = 0       # Example : 2,3,4 ...  (set it between 2 and 30)
+ignore_columns  = []    # <list> default = [], if default then uses all columns
+include_columns = []    # <list> default = [], if default then uses all columns
+num_clusters    = 3     # <int>  set 0 to optimize the number of clusters automatically
 
 
-
-# Global Settings
+# Global Settings for optimizing number of clusters
 MIN_CLUSTERS = 3
 MAX_CLUSTERS = 15
 CLUSTER_STEP_SIZE = 1
@@ -140,34 +135,60 @@ class KMeansClustering(CustomData):
         if X is None:
             return []
 
+        # check the datatype of user-defined columns
+        if not isinstance(include_columns, list):
+            raise ValueError("Variable: 'include_columns' should be <list>")
+        if not isinstance(ignore_columns, list):
+            raise ValueError("Column: 'ignore_columns' should be <list>")
+        if not isinstance(num_clusters, int):
+            raise ValueError("Column: 'num_clusters' should be <int>")
 
-        if include_columns != None:
+        ## validate user-inputs and override the columns given by user
+        features = list(X.names)    
+        if len(include_columns) > 0:
             for _ in include_columns:
                 if _ not in list(X.names):
-                    raise ValueError(str(_) + " is not present in the dataset")
+                    raise ValueError("Column: '"+str(_) + "' is not present in the dataset")
+            features = include_columns
+        
+        ## list to ignore specific columns given by user
+        features = [_f for _f in features if _f not in ignore_columns]
 
-            features = [_ for _ in include_columns if _ in list(X.names)]
-        else:
-            features = [_f for _f in X.names if _f not in ignore_columns]
+        ## handle columns with missing values 
+        ignore_ = [] 
+        X_df = X.to_pandas()
+        for col in features:
+            # label encode categorical columns
+            # refer - https://github.com/h2oai/driverlessai-recipes/pull/68#discussion_r365133392
+            
+            if X_df[col].dtype == "object": 
+                X_df[f"{col}_enc"] = LabelEncoder().fit_transform(X_df[col].to_numpy())
+                ignore_.append(col)
 
+            miss_percent = X_df[col].isna().sum() / X_df.shape[0]
+            if miss_percent >= 0.3: # ignore columns having more than 30% missing values
+                ignore_.append(col)
+            elif miss_percent > 0.0: # impute by mean for other columns with missing values
+                X_df[col] = X_df[col].fillna(X_df[col].mean())
+
+        features = [f for f in features if f not in ignore_]
+        features += [_f for _f in X_df.columns if "_enc" in _f]
         if len(features) == 0:
-            raise ValueError("Unable to cluster: No Features Selected")
+            raise ValueError("Unable to cluster: No useful features available")
 
-        # Restrict features to numerical for now
-        X_clust = X[:, features][:, [int, float]].to_numpy()
+        X_clust = X_df[features].values
 
         # Apply min max scaling
         X_clust = MinMaxScaler().fit_transform(X_clust)
 
-        # Go trhough possible numbers of clusters
+        # Go through possible numbers of clusters
         best_score = None
         best_n_clust = None
         best_clust_ids = None
 
         ## if number of clusters is pre-defined by user, then dont find the optimal
         if num_clusters > 1:
-            n_clusters = num_clusters
-            model = KMeans(n_clusters=n_clusters, n_jobs=NUM_JOBS).fit(X_clust)
+            model = KMeans(n_clusters=num_clusters, n_jobs=NUM_JOBS).fit(X_clust)
             clust_ids = model.predict(X_clust)
             score = my_davies_bouldin_score(
                 X_clust,
