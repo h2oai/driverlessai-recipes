9fc33cc12e213258b0dfe22511dc59d7139cf98b
Michelle Tanco <michelle.tanco@h2o.ai>
2020-03-15 09:47:49 -0700

    added checks that file exists

--- data/load_sas7bdat.py
+++ data/load_sas7bdat.py
@@ -14,6 +14,7 @@ from h2oaicore.data import CustomData
 import datatable as dt
 import numpy as np
 import pandas as pd
+import os
 
 _global_modules_needed_by_name = ['sas7bdat']
 from sas7bdat import SAS7BDAT
@@ -49,15 +50,18 @@ class KMeansClustering(CustomData):
 		if not isinstance(file_names, list):
 			raise ValueError("Column: 'file_names' should be <list>")
 
-		# TODO: add checs that files exist
+		# TODO: add checks that files exist
 		data_sets = {}
 
 		for f in file_names:
-		    full_path_to_file = dai_file_path + f
+		    full_path_to_file = os.path.join(dai_file_path, f)
+
+			if not os.path.exists(full_path_to_file):
+				raise ValueError("File <<" + full_path_to_file + ">> does not exists!")
+			
 		    with SAS7BDAT(full_path_to_file, skip_header=False) as reader:
 		        X = reader.to_data_frame()
 		        print(X.head())
 		        data_sets.update({f: X})
 
 		return data_sets
-
