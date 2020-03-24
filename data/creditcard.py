4f258fed989a173f4bfc0f3c5605241d8a266496
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-03-23 23:11:27 -0700

    Add simple modify data recipe for credit card

---
+++ data/creditcard.py
@@ -0,0 +1,22 @@
+"""Modify credit card dataset"""
+
+from typing import Union, List
+from h2oaicore.data import CustomData
+import datatable as dt
+import numpy as np
+import pandas as pd
+
+
+class CreditCardData(CustomData):
+
+    @staticmethod
+    def create_data(X: dt.Frame = None) -> Union[str, List[str],
+                                                 dt.Frame, List[dt.Frame],
+                                                 np.ndarray, List[np.ndarray],
+                                                 pd.DataFrame, List[pd.DataFrame]]:
+
+        if X is None:
+            return None
+        X[:, 'default payment next month leak'] = X[:, 'default payment next month']
+
+        return X
