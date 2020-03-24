9a69e74982e25475d5f3ad35da8e43cd42c31e37
Gregory Kanevsky <grigory93@users.noreply.github.com>
2020-03-24 15:52:45 -0500

    bind columns from two datasets, e.g. X with features and Y with target

---
+++ data/livecode/bind_X_and_y.py
@@ -0,0 +1,10 @@
+# dataset with target
+y_file_name = "./tmp/877bbf3a-6557-11ea-a946-0242ac110002/class_f50k_X_y_train.csv.1584123720.3251045.bin" 
+
+target_col = "y" # target column name
+
+y = dt.fread(y_file_name)
+
+X.cbind(y[target_col])
+
+return X 
