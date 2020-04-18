fe0c088c14f9c674064883e48b4be8749c0e1145
Arno Candel <arno.candel@gmail.com>
2020-04-17 17:19:44 -0700

    Automatically set threshold for Precision, Recall, False Discovery Rate.
    Use threshold for optimal F1.

--- scorers/classification/recall.py
+++ scorers/classification/recall.py
@@ -1,33 +1,45 @@
-"""Recall: `TP / (TP + FN)`. Binary uses threshold of 0.5 (please adjust), multiclass uses argmax to assign labels."""
+"""Weighted Recall: `TP / (TP + FN)` at threshold for optimal F1 Score."""
 import typing
 import numpy as np
-from h2oaicore.metrics import CustomScorer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import recall_score
+from h2oaicore.metrics import CustomScorer, prep_actual_predicted
+from sklearn.preprocessing import LabelEncoder, label_binarize
+from sklearn.metrics import precision_score
+import h2o4gpu.util.metrics as daicx
 
 
-class recall(CustomScorer):
-    _description = "Recall: `tp / (tp + fn)`"
+class RecallScorer(CustomScorer):
     _binary = True
     _multiclass = True
     _maximize = True
-    _perfect_score = 1
-    _display_name = "Recall"
-    _threshold = 0.5  # Example only, should be adjusted based on domain knowledge and other experiments
+    _threshold_optimizer = "f1"
+
+    @staticmethod
+    def _metric(tp, fp, tn, fn):
+        return tp / (tp + fn)
+
 
     def score(self,
               actual: np.array,
               predicted: np.array,
               sample_weight: typing.Optional[np.array] = None,
               labels: typing.Optional[np.array] = None) -> float:
-        lb = LabelEncoder()
-        labels = lb.fit_transform(labels)
-        actual = lb.transform(actual)
-        method = "binary"
-        if len(labels) > 2:
-            predicted = np.argmax(predicted, axis=1)
-            method = "micro"
-        else:
-            predicted = (predicted > self._threshold)
 
-        return recall_score(actual, predicted, labels=labels, average=method, sample_weight=sample_weight)
+        if sample_weight is not None:
+            sample_weight = sample_weight.ravel()
+        if len(actual) == 1:
+            return 1.0
+        enc_actual, enc_predicted, labels = prep_actual_predicted(actual, predicted, labels)
+        cm_weights = sample_weight if sample_weight is not None else None
+
+        # multiclass
+        if enc_predicted.shape[1] > 1:
+            enc_predicted = enc_predicted.ravel()
+            enc_actual = label_binarize(enc_actual, labels).ravel()
+            cm_weights = np.repeat(cm_weights, predicted.shape[1]).ravel() if cm_weights is not None else None
+            assert enc_predicted.shape == enc_actual.shape
+            assert cm_weights is None or enc_predicted.shape == cm_weights.shape
+
+        cms = daicx.confusion_matrices(enc_actual.ravel(), enc_predicted.ravel(), sample_weight=cm_weights)
+        cms = cms.loc[cms[[self.__class__._threshold_optimizer]].idxmax()]  # get row(s) for optimal metric defined above
+        cms['metric'] = cms[['tp', 'fp', 'tn', 'fn']].apply(lambda x: self.__class__._metric(*x), axis=1, raw=True)
+        return cms['metric'].mean()  # in case of ties
