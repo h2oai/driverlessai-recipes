bbd09831866872ca7a5b11a30469b9298a9c6810
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-04-20 18:22:48 -0700

    Change names for things added natively to DAI

--- scorers/classification/recall.py
+++ scorers/classification/recall.py
@@ -7,7 +7,7 @@ from sklearn.metrics import precision_score
 import h2o4gpu.util.metrics as daicx
 
 
-class RecallScorer(CustomScorer):
+class MyRecallScorer(CustomScorer):
     _binary = True
     _multiclass = True
     _maximize = True
