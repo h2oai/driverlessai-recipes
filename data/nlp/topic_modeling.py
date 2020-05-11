760babbf93d3ca83c769372ded17fc79feebd856
SudalaiRajkumar <ssraj.rox@gmail.com>
2020-05-11 16:25:16 +0530

    adding lsi kmeans for topic modeling

--- data/nlp/topic_modeling.py
+++ data/nlp/topic_modeling.py
@@ -4,11 +4,17 @@ from h2oaicore.data import CustomData
 import datatable as dt
 import numpy as np
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.preprocessing import Normalizer
+from sklearn.cluster import KMeans
 
 # number of topics in the topic model
 n_topics = 4
 # text column name on which the topic modeling will be run
 text_colname = "text"
+# Name of the topic modeling algo - "LDA", "TFIDF_KMEANS", "LSI_KMEANS"
+tm_algo_name = "LDA"
 # output dataset name
 output_dataset_name = "df_topic_modeling"
 # number of top words to be represented in the column name
@@ -35,27 +41,52 @@ class LdaTopicsClass(CustomData):
 
         X = dt.Frame(X)
         documents = X.to_pandas()[text_colname].astype(str).fillna("NA").values
-        new_X = [[w for w in simple_preprocess(doc, deacc=True) if w not in stop_words] for doc in documents]
 
-        bigram = gensim.models.Phrases(new_X, min_count=5, threshold=10)
-        bigram_mod = gensim.models.phrases.Phraser(bigram)
-        new_X = [bigram_mod[d] for d in new_X]
+        if tm_algo_name == "LDA":
+            new_X = [[w for w in simple_preprocess(doc, deacc=True) if w not in stop_words] for doc in documents]
+            bigram = gensim.models.Phrases(new_X, min_count=5, threshold=10)
+            bigram_mod = gensim.models.phrases.Phraser(bigram)
+            new_X = [bigram_mod[d] for d in new_X]
 
-        dictionary = corpora.Dictionary(new_X)
-        new_X = [dictionary.doc2bow(doc) for doc in new_X]
+            dictionary = corpora.Dictionary(new_X)
+            new_X = [dictionary.doc2bow(doc) for doc in new_X]
 
-        model = gensim.models.ldamodel.LdaModel(new_X,
-                                                num_topics=n_topics,
-                                                id2word=dictionary,
-                                                passes=10,
-                                                alpha="auto",
-                                                per_word_topics=True,
-                                                random_state=2019)
-        df = pd.concat([X.to_pandas(), pd.DataFrame(model.inference(new_X)[0])], axis=1)
+            model = gensim.models.ldamodel.LdaModel(new_X,
+                                                    num_topics=n_topics,
+                                                    id2word=dictionary,
+                                                    passes=10,
+                                                    alpha="auto",
+                                                    per_word_topics=True,
+                                                    random_state=2019)
+            df = pd.concat([X.to_pandas(), pd.DataFrame(model.inference(new_X)[0])], axis=1)
+            topics = model.print_topics(num_words=n_words_colname)
+            topic_names = ["_".join([v.split("*")[1].strip('"') for v in x[1].split(" + ")]) for x in topics]
+            df.columns = list(X.names) + topic_names
 
-        topics = model.print_topics(num_words=n_words_colname)
-        topic_names = ["_".join([v.split("*")[1].strip('"') for v in x[1].split(" + ")]) for x in topics]
-        df.columns = list(X.names) + topic_names
+        elif tm_algo_name in ["TFIDF_KMEANS", "LSI_KMEANS"]:
+            tfidf_vec = TfidfVectorizer(min_df=3, max_df=0.5, stop_words='english')
+            new_X = tfidf_vec.fit_transform(documents)
+
+            if tm_algo_name == "LSI_KMEANS":
+                svd = TruncatedSVD(n_components=30)
+                new_X = svd.fit_transform(new_X)
+                normalizer = Normalizer(copy=False)
+                new_X = normalizer.fit_transform(new_X)
+
+            km = KMeans(n_clusters=n_topics, init='k-means++', max_iter=100, n_init=3)
+            new_X = pd.DataFrame(km.fit_transform(new_X))
+            df = pd.concat([X.to_pandas(), new_X], axis=1)
+
+            if tm_algo_name == "TFIDF_KMEANS":
+                order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+            elif tm_algo_name == "LSI_KMEANS":
+                original_space_centroids = svd.inverse_transform(km.cluster_centers_)
+                order_centroids = original_space_centroids.argsort()[:, ::-1]
+
+            terms = tfidf_vec.get_feature_names()
+            topic_names = ["_".join([terms[ind] for ind in order_centroids[i, :n_words_colname]])
+                               for i in range(n_topics)]
+            df.columns = list(X.names) + topic_names
 
         temp_path = os.path.join(config.data_directory, config.contrib_relative_directory)
         os.makedirs(temp_path, exist_ok=True)
