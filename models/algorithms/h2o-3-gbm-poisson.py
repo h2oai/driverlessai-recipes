012bc74d1694b5aa562ee658989bf2b236f39277
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-05-26 11:55:38 -0700

    update h2o version

--- models/algorithms/h2o-3-gbm-poisson.py
+++ models/algorithms/h2o-3-gbm-poisson.py
@@ -8,7 +8,7 @@ from h2o.estimators.gbm import H2OGradientBoostingEstimator
 
 import numpy as np
 
-_global_modules_needed_by_name = ['h2o==3.26.0.1']
+_global_modules_needed_by_name = ['h2o==3.30.0.3']
 import h2o
 import os
 
