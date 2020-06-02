26be12e01bd93ec4757a5d31d40b03477f0dbee0
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-06-02 15:37:48 -0700

    Bump uszipcode package versions so compatible with DAI flake8 requirements

--- transformers/augmentation/uszipcode_features_database.py
+++ transformers/augmentation/uszipcode_features_database.py
@@ -4,7 +4,7 @@ from h2oaicore.systemutils import make_experiment_logger, loggerinfo, loggerwarn
 import datatable as dt
 import numpy as np
 
-_global_modules_needed_by_name = ['pycodestyle==2.5.0', 'uszipcode==0.2.2']
+_global_modules_needed_by_name = ['pycodestyle==2.6.0', 'uszipcode==0.2.4']
 from uszipcode import SearchEngine
 
 
