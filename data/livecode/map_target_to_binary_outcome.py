50be521df43fa08f2c92eff4ceecdb723fcfa046
Gregory Kanevsky <grigory93@users.noreply.github.com>
2020-06-24 12:21:42 -0500

    Create map_target_to_binary_outcome.py
    livecode recipe for mapping multi-nominal target to binary one

---
+++ data/livecode/map_target_to_binary_outcome.py
@@ -0,0 +1,18 @@
+# Maps multi-nominal target (outcome) to binomial target column by
+# binding new column to a dataset (new dataset will be created).
+# For example, use when working with multi-nominal classifier and want 
+# to see if binomial model may be preferred or compliment use case.
+
+old_target_name = 'intake_type'
+new_target_name = 'binary_target'
+value_to_map_to_true = 'STRAY'
+binary_outcomes = (False, True)
+
+# setup filter
+binary_outcome_filter = (dt.f[old_target_name] == value_to_map_to_true)
+
+# create new target
+X[binary_outcome_filter, new_target_name] = binary_outcomes[1]
+X[~binary_outcome_filter, new_target_name] = binary_outcomes[0]
+
+return X  # return dt.Frame, pd.DataFrame, np.ndarray or a list of those
