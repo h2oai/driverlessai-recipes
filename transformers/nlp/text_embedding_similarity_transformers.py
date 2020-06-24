fde165168f46046170020063394a6f8988d82c00
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-06-24 12:11:16 -0700

    flair needs different regexp than DAI. Can set ignore_package_version to include regexp and gensim to make work.

--- transformers/nlp/text_embedding_similarity_transformers.py
+++ transformers/nlp/text_embedding_similarity_transformers.py
@@ -7,7 +7,7 @@ import math
 
 
 class EmbeddingSimilarityTransformer(CustomTransformer):
-    _modules_needed_by_name = ['regex==2019.12.17', 'flair==0.4.1', 'segtok==1.5.7']
+    _modules_needed_by_name = ["gensim==3.8.0", 'regex==2019.12.17', 'flair==0.4.1', 'segtok==1.5.7']
     _is_reproducible = False
     _can_use_gpu = True
     _repl_val = 0
