1739d93ef4c212e1df14030613bd123f34ee0b87
Gregory Kanevsky <grigory93@users.noreply.github.com>
2020-06-26 13:52:53 -0500

    option to drop old target column

--- data/livecode/map_target_to_binary_outcome.py
+++ data/livecode/map_target_to_binary_outcome.py
@@ -7,6 +7,7 @@ old_target_name = 'outcome_type'
 new_target_name = 'is_adoption_target'
 value_to_map_to_true = 'ADOPTION'
 binary_outcomes = (False, True)
+drop_old_target = False
 
 # setup filter
 binary_outcome_filter = (dt.f[old_target_name] == value_to_map_to_true)
@@ -14,5 +15,9 @@ binary_outcome_filter = (dt.f[old_target_name] == value_to_map_to_true)
 # create new target
 X[binary_outcome_filter, new_target_name] = binary_outcomes[1]
 X[~binary_outcome_filter, new_target_name] = binary_outcomes[0]
+
+# drop old target
+if drop_old_target:
+  del X[:, old_target_name]
 
 return X  # return dt.Frame, pd.DataFrame, np.ndarray or a list of those
