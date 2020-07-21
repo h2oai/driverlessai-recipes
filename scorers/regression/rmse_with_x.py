ac4977ae899cce99bbcb7a0272a9d1c424f55531
Arno Candel <arno.candel@gmail.com>
2020-07-21 15:05:24 -0700

    Add file for previous commit.

---
+++ scorers/regression/rmse_with_x.py
@@ -0,0 +1,21 @@
+"""Custom RMSE Scorer that also gets X (original features) - for demo/testing purposes only"""
+from h2oaicore.metrics import RmseScorer, CustomScorer
+import numpy as np
+import datatable as dt
+import typing
+
+class RmseWithXScorer(CustomScorer):
+    _regression = True
+    _needs_X = True
+
+    def score(
+            self,
+            actual: np.array,
+            predicted: np.array,
+            sample_weight: typing.Optional[np.array] = None,
+            labels: typing.Optional[np.array] = None,
+            X: typing.Optional[dt.Frame] = None,
+            **kwargs) -> float:
+        assert X  # for testing
+        return RmseScorer().score(actual, predicted, sample_weight, labels, **kwargs)
+
