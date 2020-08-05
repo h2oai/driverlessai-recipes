219878148f0fbba2ed146033911c4abc2754e52d
JasminePParekh <jasmine.p.parekh@gmail.com>
2020-08-05 11:22:47 -0700

    Made arguments changes

--- transformers/numeric/round_transformer.py
+++ transformers/numeric/round_transformer.py
@@ -10,7 +10,7 @@ class MyRoundTransformer(CustomTransformer):
 
     @staticmethod
     def get_parameter_choices():
-        return {'op_params': {'decimals': (MojoType.INT32, [1, 2, 3])}}
+        return {"decimals": [1, 2, 3]}
 
     @property
     def display_name(self):
@@ -26,9 +26,31 @@ class MyRoundTransformer(CustomTransformer):
     def transform(self, X: dt.Frame):
         return np.round(X.to_numpy(), decimals=self.decimals)
 
-    mojo = True
-    from h2oaicore.mojo import MojoWriter, MojoFrame
+    _mojo = True
+    from h2oaicore.mojo import MojoWriter, MojoFrame, MojoType, MojoColumn
 
+    def to_mojo(self, mojo: MojoWriter, iframe: MojoFrame, group_uuid=None, group_name=None):
+        import uuid
+        group_uuid = str(uuid.uuid4())
+        group_name = self.__class__.__name__
+        kws = dict()
+        kws["op_name"] = "round"
+        custom_param = dict()
+        custom_param["decimals"] = (MojoType.INT32, self.decimals)
+        kws["op_params"] = custom_param
+        from h2oaicore.mojo_transformers import MjT_CustomOp
+        from h2oaicore.mojo_transformers_utils import AsType
+        xnew = iframe[self.input_feature_names]
+        oframe = MojoFrame()
+        for col in xnew:
+            ocol = MojoColumn(name=col.name, dtype=col.type)
+            ocol_frame = MojoFrame(columns=[ocol])
+            #mojo += MjT_CustomOp(iframe=MojoFrame(columns=[col]), oframe=ocol_frame, group_uuid=group_uuid, group_name=group_name, **kws)
+            mojo += MjT_CustomOp(MojoFrame(columns=[col]), ocol_frame, group_uuid, group_name, **kws)
+            oframe += ocol
+        oframe = AsType(dtype_global()).write_to_mojo(mojo, oframe, group_uuid=group_uuid, group_name=group_name)
+        return oframe
+    
     def to_mojo(self, mojo: MojoWriter, iframe: MojoFrame, group_uuid=None, group_name=None):
         
         import uuid
