9db06a7ab9b1c5714eeb6fa21856e26de5a3366d
Gregory Kanevsky <grigory93@gmail.com>
2020-08-09 19:07:32 -0500

    fix file names

--- data/livecode/gen-readme.py
+++ data/livecode/gen-readme.py
@@ -36,20 +36,26 @@ for f in sorted(livecode_files):
                 if f[-3:] == ".py":
                         docstring = get_file_docstring(os.path.join(livecode_path, f)) or \
                                     "please add description"
-                        what = "[" + f + "](" + livecode_path + "/" + f + ")"
+                        what = "[" + f.replace("_", "\_") + "](" + "./" + f + ")"
                         print_offset(depth + 1, "%s [%s]" % (what, docstring), ret)
                         count += 1
 
-print("""# Recipes for H2O Driverless AI
+print("""# Live Code Templates for H2O Driverless AI Datasets
 
 ## About Driverless AI and BYOR 
 See main [README](https://github.com/h2oai/driverlessai-recipes/README.md)
 
 ## About Data Recipes
 Driverless AI allows you to create a new dataset by modifying an existing dataset with a data recipe. 
-When inside **Dataset Details** page click the **Modify by Recipe** button in the top right portion of the UI, then click **Live Code** from the submenu that appears.
-The Dialog Box that appears will feature an editor to enter the code for the data recipe you want to use to modify the dataset. The list of templates below are designed
-for the live code applications with certain modifications specific to the dataset they apply to.
+When inside **Dataset Details** page:
+
+* click the **Modify by Recipe** button in the top right portion of the UI
+* click **Live Code** from the submenu that appears
+* enter the code inside the **Dialog Box** that appears featurng an editor 
+* enter the code for the data recipe you want to use to modify the dataset. 
+
+The list below contains the live code templates for various applications of data recipes. Each template is designed and documented to be applied 
+after modifications specific to a dataset it applies to.
 
 ## Reference Guide
 * [Adding a Data Recipe](http://docs.h2o.ai/driverless-ai/latest-stable/docs/userguide/custom-recipes-data-recipes.html#adding-a-data-recipe)
