027fa821389bcd43b2f9f61165fb4f248b597853
Gregory Kanevsky <grigory93@gmail.com>
2020-08-10 22:25:33 -0500

    fix parsing logic

--- data/livecode/cast_columns_to_numeric.py
+++ data/livecode/cast_columns_to_numeric.py
@@ -1,10 +1,13 @@
-# Make almost numeric fields numeric by ignoring strings and representing them as missing
+# Cast columns with mostly numeric values to new numeric columns.
+# Certain percentage (threshold) of non-numeric values per column allowed before casting.
+# Non-numeric values are ignored and become nulls inside new columns.
 #
 # Specification:
 # Inputs:
 #   X: datatable - primary data set
 # Parameters:
-#   threshold: numeric - threshold for percantage of allowed non-numeric values per column before conversion
+#   columns: list of column names - columns to cast to numeric. If None (default) then all character columns
+#   threshold: numeric - threshold for percentage of allowed non-numeric values per column before conversion
 #   name_suffix: string - suffix to add to the name of new numeric column converted from the original
 # Output:
 #   dataset with added numeric columns derived from character columns
@@ -13,15 +16,25 @@ import pandas as pd
 import numpy as np
 
 # percent of values allowed to dismiss as non-numeric (missing) after conversion
+columns = None
 threshold = 0.01
 name_suffix = '_to_numeric'
 
-df = X[:, dt.f[str]].to_pandas()
+# select columns to cast and convert to pandas frame
+if columns is None:
+    columns = X[:, dt.f[str]].names
+df = X[:, columns].to_pandas()
 
-columns = list(df)
+# cast to numeric
+nrows = df.shape[0]
+if nrows == 0:
+    return None
+
+# check if percentage of non-numerics is low and then cast
 for c in columns:
-    fi = df[c].apply(lambda x: x.isnumeric())
-    if sum(fi==False)/len(fi) <= threshold:
+    # special care taken of negative values
+    percent_of_non_numeric = sum(df[c].apply(lambda x: not (x[1:].isnumeric() if x.startswith("-") else x.isnumeric()))) / nrows
+    if percent_of_non_numeric <= threshold:
         X[c + name_suffix] = pd.to_numeric(df[c], errors='coerce')
 
 return X
