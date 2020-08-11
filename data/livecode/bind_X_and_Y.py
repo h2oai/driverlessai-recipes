a79a66737ca1bee62721308c56aefa5cf93bcb47
Gregory Kanevsky <grigory93@gmail.com>
2020-08-11 18:11:49 -0500

    add bind and X and Y template

--- data/livecode/bind_X_and_Y.py
+++ data/livecode/bind_X_and_Y.py
@@ -1,20 +1,26 @@
-# Livecode for binding 2 datasets with the same number of rows, e.g.
-# one dataset with features and another dataset has target.
-# Recipe won't perform any joins/mapping but rather stitch
-# 2 datasets' rows together.
+# Template for binding columns from 2 datasets with the same number of rows.
+# Recipe won't perform any joins/mapping but rather stitch 2 datasets together into wider dataset with
+# the same number of rows and columns from both.
+#
+# Specification:
+# Inputs:
+#   X: datatable - primary dataset
+#   Y_name: datatable - dataset to bind with
+# Parameters:
+#   None
+# Output:
+#   dataset containing all rows from both datasets
 
 # find location of the dataset file by going to DETAILS where it's displayed
 # on top under dataset name
-y_file_name = "./tmp/877bbf3a-6557-11ea-a946-0242ac110002/class_f50k_X_y_train.csv.1584123720.3251045.bin"
-y = dt.fread(y_file_name)
-if y.shape[0] != X.shape[0]:
+Y_name = "./tmp/877bbf3a-6557-11ea-a946-0242ac110002/class_f50k_X_y_train.csv.1584123720.3251045.bin"
+
+new_dataset_name = "new_dataset_name_after_cbind"
+
+Y = dt.fread(Y_name)
+if Y.shape[0] != X.shape[0]:
     raise ValueError("Datasets must have equal number of rows")
 
-# target column name
-target_col = "y"
-if not target_col in y.names:
-    raise ValueError("Target column not found in y dataset")
+X.cbind(Y)
 
-X.cbind(y[target_col])
-
-return X
+return {new_dataset_name: X}
---
+++ data/livecode/bind_X_and_target_y.py
@@ -0,0 +1,35 @@
+# Template for binding dataset and target from another dataset with the same number of rows,
+# e.g. one dataset has features and another contains target.
+# Recipe won't perform any joins/mapping but rather stitch 2 datasets together into wider dataset with
+# the same number of rows and columns from 1st dataset plus target from another.
+#
+# Specification:
+# Inputs:
+#   X: datatable - primary dataset
+#   y_name: datatable - dataset to bind with
+# Parameters:
+#   target_col: string - target name
+# Output:
+#   dataset containing all rows from both datasets
+
+# find location of the dataset file by going to DETAILS where it's displayed
+# on top under dataset name
+y_name = "./tmp/877bbf3a-6557-11ea-a946-0242ac110002/class_f50k_X_y_train.csv.1584123720.3251045.bin"
+# target column name
+target_col = "y"
+
+new_dataset_name = "new_dataset_name_after_cbind_y"
+
+y = dt.fread(y_name)
+if y.shape[0] != X.shape[0]:
+    raise ValueError("Datasets must have equal number of rows.")
+
+if not target_col in y.names:
+    raise ValueError(f"Target column '{target_col}' not found in y dataset.")
+
+if target_col in X.names:
+    raise ValueError(f"Target column '{target_col}' found in X dataset.")
+
+X.cbind(y[target_col])
+
+return {new_dataset_name: X}
