fd269d41432ff3f35ec2b9288639d474dbfc94a1
Gregory Kanevsky <grigory93@gmail.com>
2020-08-21 15:12:11 -0500

    fix parsing and comments

--- data/livecode/parse_string_to_datetime.py
+++ data/livecode/parse_string_to_datetime.py
@@ -1,4 +1,6 @@
-# Parse and convert string column to date
+# Parse and convert string column to date.
+# This example converts string in the format `MMMMYY` to `MMMM-YY-DD`.
+# Please adjust code to the format you expect to find in your data.
 #
 # Specification:
 # Inputs:
@@ -17,14 +19,14 @@ new_dataset_name = "new_dataset_name_with_parsed_date"
 # remove rows with non-numierc values
 del X[~X[col_name].to_pandas()[col_name].str.isnumeric(), :]
 
-# remove rows with
+# remove rows with less or greater than 6 characters long (not 'MMMMYY' format)
 del X[X[col_name].to_pandas()[col_name].str.strip().str.len() != 6, :]
 
 # convert to pandas for string processing
 X_pd = X[col_name].to_pandas()
 
-# strip of spaces
-X[col_name] = X_pd[col_name].str.strip()
+# strip of both leading and trailing spaces
+X_pd[col_name] = X_pd[col_name].str.strip()
 
 # parse to date
 X[date_col_name] = X_pd[col_name].str[0:4] + "-" + X_pd[col_name].str[4:] + "-01"
