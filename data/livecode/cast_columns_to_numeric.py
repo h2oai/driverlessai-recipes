f8740af24255a0de023abbc0c084b42de310be85
Gregory Kanevsky <grigory93@gmail.com>
2020-08-30 16:03:31 -0500

    refactored to use datatable

--- data/livecode/cast_columns_to_numeric.py
+++ data/livecode/cast_columns_to_numeric.py
@@ -1,42 +1,49 @@
 # Cast columns with mostly numeric values to new numeric columns.
-# Certain percentage (threshold) of non-numeric values per column allowed before casting.
-# Non-numeric values are ignored and become nulls inside new columns.
+# Fraction of non-numeric values (per column) equal or below a threshold defined is allowed before casting.
+# Non-numeric values are ignored and mapped to null value.
 #
 # Specification:
 # Inputs:
 #   X: datatable - primary data set
 # Parameters:
 #   columns: list - columns to cast to numeric. If None (default) then all character columns
-#   threshold: numeric - threshold for percentage of allowed non-numeric values per column before conversion
+#   threshold_fraction_non_numeric: numeric - threshold for percentage of allowed non-numeric values per column before conversion
+#   in_place: bool - indicates if casting to numeric takes place in place or by adding new column
 #   name_suffix: string - suffix to add to the name of new numeric column converted from the original
 # Output:
 #   dataset with added numeric columns derived from character columns
 
-import pandas as pd
-import numpy as np
-
-# percent of values allowed to dismiss as non-numeric (missing) after conversion
 columns = None
-threshold = 0.01
+threshold_fraction_non_numeric = 0.01
+in_place = False
 name_suffix = '_num'
 
 new_dataset_name = "new_dataset_name_after_casting_to_num"
 
-# select columns to cast and convert to pandas frame
-if columns is None:
-    columns = X[:, dt.f[str]].names
-df = X[:, columns].to_pandas()
-
-# cast to numeric
-nrows = df.shape[0]
+# check for empty frame
+nrows = X.nrows
 if nrows == 0:
     return None
 
-# check if percentage of non-numerics is low and then cast
+# select columns to cast and convert to pandas frame
+if columns is None:
+    columns = X[:, dt.f[str]].names
+temp = X[:, columns]
+
 for c in columns:
-    # special care taken of negative values
-    percent_of_non_numeric = sum(df[c].apply(lambda x: not (x[1:].isnumeric() if x.startswith("-") else x.isnumeric()))) / nrows
-    if percent_of_non_numeric <= threshold:
-        X[c + name_suffix] = pd.to_numeric(df[c], errors='coerce')
+    # count nulls before casting
+    null_count = temp[dt.isna(dt.f[c]),:].nrows
+    # cast
+    temp[c] = float
+    # compute fraction of non-numeric values
+    fraction_non_numeric = (temp[dt.isna(dt.f[c]), :].nrows - null_count) / temp.nrows
+    # check if fraction is below pre-defined threshold
+    if fraction_non_numeric <= threshold_fraction_non_numeric:
+        assert temp[c].ltypes[0] == dt.ltype.real, f"wrong type: {temp[c].ltypes[0]}"
+        # update or augment dataset with numeric values
+        if in_place:
+            X[c] = temp[c]
+        else:
+            X[c + name_suffix] = temp[c]
 
 return {new_dataset_name: X}
