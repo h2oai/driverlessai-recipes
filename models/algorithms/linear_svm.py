bca7da82aa8052036d15aa6f2668267247a3912a
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-11-20 15:56:38 -0800

    Add basic_impute to linear_svm to avoid assert from dt not removing nans

--- models/algorithms/linear_svm.py
+++ models/algorithms/linear_svm.py
@@ -143,14 +143,7 @@ class LinearSVMModel(CustomModel):
                               dual=self.params["dual"], random_state=self.random_state)
         self.means = dict()
         self.standard_scaler = StandardScaler()
-        for col in X.names:
-            XX = X[:, col]
-            self.means[col] = XX.mean1()
-            if self.means[col] is None:
-                self.means[col] = 0
-            XX.replace(None, self.means[col])
-            X[:, col] = XX
-            assert X[dt.isna(dt.f[col]), col].nrows == 0
+        X = self.basic_impute(X)
         X = X.to_numpy()
         X = self.standard_scaler.fit_transform(X)
         model.fit(X, y, sample_weight=sample_weight)
@@ -166,18 +159,35 @@ class LinearSVMModel(CustomModel):
                                   importances=importances.tolist(),  # abs(model.coef_[0])
                                   iterations=0)
 
-    def predict(self, X, **kwargs):
-        X = dt.Frame(X)
+    def basic_impute(self, X):
+        # scikit extra trees internally converts to np.float32 during all operations,
+        # so if float64 datatable, need to cast first, in case will be nan for float32
+        from h2oaicore.systemutils import update_precision
+        X = update_precision(X, data_type=np.float32, override_with_data_type=True, fixup_almost_numeric=True)
+        # Replace missing values with a value smaller than all observed values
+        if not hasattr(self, 'min'):
+            self.min = dict()
         for col in X.names:
             XX = X[:, col]
-            XX.replace(None, self.means[col])
+            if col not in self.min:
+                self.min[col] = XX.min1()
+                if self.min[col] is None or np.isnan(self.min[col]) or np.isinf(self.min[col]):
+                    self.min[col] = -1e10
+                else:
+                    self.min[col] -= 1
+            XX.replace([None, np.inf, -np.inf], self.min[col])
             X[:, col] = XX
+            assert X[dt.isna(dt.f[col]), col].nrows == 0
+        return X
 
+    def predict(self, X, **kwargs):
+        X = dt.Frame(X)
+        X = self.basic_impute(X)
+        X = X.to_numpy()
         pred_contribs = kwargs.get('pred_contribs', None)
         output_margin = kwargs.get('output_margin', None)
 
         model, _, _, _ = self.get_model_properties()
-        X = X.to_numpy()
         X = self.standard_scaler.transform(X)
         if not pred_contribs:
             if self.num_classes == 1:
