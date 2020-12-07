0769367f41fdd2ad886b0038dcc0c11b1c380e7d
Jonathan C. McKinney <pseudotensor@gmail.com>
2020-12-07 00:57:32 -0800

    Add multiple data test

--- data/data_test_multiple.py
+++ data/data_test_multiple.py
@@ -1,4 +1,4 @@
-"""Create test dataset"""
+"""Create multiple dataset test"""
 import uuid
 from typing import Union, List
 from h2oaicore.data import CustomData
@@ -8,9 +8,7 @@ import pandas as pd
 from h2oaicore.systemutils import user_dir
 
 
-class TestData(CustomData):
-    url = "http://data.un.org/_Docs/SYB/CSV/SYB63_226_202009_Net%20Disbursements%20from%20Official%20ODA%20to%20Recipients.csv"
-
+class TestDataMultiple(CustomData):
     @staticmethod
     def create_data(X: dt.Frame = None) -> Union[str, List[str],
                                                  dt.Frame, List[dt.Frame],
@@ -23,7 +21,10 @@ class TestData(CustomData):
         temp_path = os.path.join(user_dir(), config.contrib_relative_directory, "testdata_%s" % str(uuid.uuid4()))
         os.makedirs(temp_path, exist_ok=True)
 
-        link = TestData.url
-        file = download(link, dest_path=temp_path)
+        link = "http://data.un.org/_Docs/SYB/CSV/SYB63_226_202009_Net%20Disbursements%20from%20Official%20ODA%20to%20Recipients.csv"
+        output_file1 = download(link, dest_path=temp_path)
 
-        return file
+        link = "http://data.un.org/_Docs/SYB/CSV/SYB63_223_202009_Net%20Disbursements%20from%20Official%20ODA%20from%20Donors.csv"
+        output_file2 = download(link, dest_path=temp_path)
+
+        return [output_file1, output_file2]
