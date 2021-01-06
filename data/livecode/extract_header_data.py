0b48201a0f772433df1fe92ccfc6d8e2e97a9d4b
Gregory Kanevsky <grigory93@users.noreply.github.com>
2021-01-05 18:04:09 -0600

    template to filter columns and collapse dataset to header level rows

---
+++ data/livecode/extract_header_data.py
@@ -0,0 +1,46 @@
+# Extract header data from detailed (line item level) dataset by filtering top level 
+# columns and then removing duplicate rows.
+#
+# Specification:
+# Inputs:
+#   X: datatable - primary dataset
+# Parameters:
+#   cols_keep: list of column names - columns to keep for header dataset
+#   cols_regex_keep: str - regular expression pattern for columns to keep
+#   cols_remove: list of column names - columns to drop from header dataset
+#   cols_regex_remove: str - regular expression pattern for column to remove
+#   add_rowcount: bool - when true a column with line item row count added to the header data
+# Output:
+#   dataset containing only columns to keep (without removed ones) and no duplicates
+
+cols_keep = []
+cols_regex_keep = None
+cols_remove = ['Measure', 'Value']
+cols_regex_remove = None
+add_rowcount = False
+
+from datatable import f, count, by
+
+new_dataset_name = "dataset_header_collapsed"
+
+cols = list(X.names)
+if cols_keep and len(cols_keep) > 0:
+  cols = cols_keep
+elif cols_regex_keep:
+  regex = re.compile(cols_regex_keep)
+  cols = list(filter(regex.match, cols))
+elif cols_remove and len(cols_remove) > 0:
+  [cols.remove(el) for el in cols_remove]
+elif cols_regex_remove:
+  regex = re.compile(cols_regex_remove)
+  cols_remove = list(filter(regex.match, cols))
+  [cols.remove(el) for el in cols_remove]
+
+X = X[:, cols]
+
+X = X[:, dt.count(), dt.by(cols)]
+
+if not add_rowcount:
+  del X[:, f[-1]]
+  
+return {new_dataset_name: X}
