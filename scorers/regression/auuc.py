d0630cd655edb41385822fafec7c519727f28b62
Vaclav Belak <vaclav@h2o.ai>
2021-02-25 14:38:41 +0100

    Adding license.

--- scorers/regression/auuc.py
+++ scorers/regression/auuc.py
@@ -23,6 +23,20 @@ class AUUC(CustomScorer):
     # https://github.com/uber/causalml/blob/v0.10.0/causalml/metrics/visualize.py
     # The functions get_cumgain and get_cumlift were copied as is (only the `self` reference was added).
     # The auuc_score was modified: the `tmle` parameter was removed since it is not used here.
+    # The get_cumgaim, get_cumlift, and auuc_score functions are licensed under the Apache 2 license:
+    #
+    # Copyright 2019 Uber Technology, Inc.
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #     http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+    # limitations under the License.
     def get_cumgain(self, df, outcome_col='y', treatment_col='w', treatment_effect_col='tau',
                     normalize=False, random_seed=42):
         """Get cumulative gains of model estimates in population.
