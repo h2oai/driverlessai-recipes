88813c5865664ae69c2602dc9ca58d9be7f2fda6
Jonathan C. McKinney <pseudotensor@gmail.com>
2021-04-30 15:21:14 -0700

    change pystan given 3 is too new for prophet and match DAI holidays patckage so prophet works. i.e. holidays==0.11.1 instead of original holidays==0.9.11 that fails for various reasons.

--- transformers/timeseries/serial_prophet_forecast.py
+++ transformers/timeseries/serial_prophet_forecast.py
@@ -31,7 +31,7 @@ class MySerialProphetTransformer(CustomTimeSeriesTransformer):
     _binary = False
     _multiclass = False
     # some package dependencies are best sequential to overcome known issues
-    _modules_needed_by_name = ['convertdate', 'pystan==3.0.2', 'fbprophet==0.7.1']
+    _modules_needed_by_name = ['convertdate', 'pystan==2.19.1.1', 'fbprophet==0.7.1']
     # _modules_needed_by_name = ['fbprophet']
     _included_model_classes = None  # ["gblinear"] for strong trends - can extrapolate
     _testing_can_skip_failure = False  # ensure tested as if shouldn't fail
