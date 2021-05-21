3de2dc22385aa6c09615aa95bed330ddeac890b6
Jonathan C. McKinney <pseudotensor@gmail.com>
2021-05-21 14:26:32 -0700

    Add another way for latest DAI to choose to isolate env, i.e. without decorator

--- data/any_env4.py
+++ data/any_env4.py
@@ -1,18 +1,15 @@
 """Modify dataset with arbitrary env"""
 
 from h2oaicore.data import CustomData
-from h2oaicore.utils import wrap_create
 
 
 class FreshEnvData(CustomData):
-    @staticmethod
     # Specify the python package dependencies.  Will be installed in order of list
-    # NOTE: Keep @wrap_create on a single line
-    # NOTE: If want to share cache across recipes, can set cache_env=True and set id=<some unique identifier, like myrecipe12345>
     # Below caches the env into "id" folder
-    #@wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], cache_env=True, file=__file__, id="myrecipe12345")
+    #isolate_env = dict(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], cache_env=True, file=__file__, id="myrecipe12345")
     # Below does not cache the env
-    @wrap_create(pyversion="3.8", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], file=__file__)
+    isolate_env = dict(pyversion="3.8", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"])
+    @staticmethod
     def create_data(X=None):
         import os
         import datatable as dt
