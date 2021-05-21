474d17899cad3ebff81f4222093a48a2b79706fe
Jonathan C. McKinney <pseudotensor@gmail.com>
2021-05-21 12:25:25 -0700

    Fix typo in comment

--- data/any_env2.py
+++ data/any_env2.py
@@ -110,8 +110,9 @@ class FreshEnvData(CustomData):
     @staticmethod
     # Specify the python package dependencies.  Will be installed in order of list
     # NOTE: Keep @wrap_create on a single line
-    # NOTE: If want to share cache across recipes, can set cache_env=True and set file=<some unique identifier, like myrecipe12345>
-    @wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], cache_env=True, file=__file__, id="myrecipe12345")
+    # NOTE: If want to share cache across recipes, can set cache_env=True and set id=<some unique identifier, like myrecipe12345>
+    #@wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], cache_env=True, file=__file__, id="myrecipe12345")
+    @wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], file=__file__)
     def create_data(X=None):
         import os
         import datatable as dt
