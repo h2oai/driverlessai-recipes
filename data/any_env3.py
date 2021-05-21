07d6eea5a7a1f98a82b43a1ef32cc4b469190448
Jonathan C. McKinney <pseudotensor@gmail.com>
2021-05-21 00:14:57 -0700

    recipe when DAI has wrap_create

---
+++ data/any_env3.py
@@ -0,0 +1,32 @@
+"""Modify dataset with arbitrary env"""
+
+from h2oaicore.data import CustomData
+from h2oaicore.utils import wrap_create
+
+
+class FreshEnvData(CustomData):
+    @staticmethod
+    # Specify the python package dependencies.  Will be installed in order of list
+    # NOTE: Keep @wrap_create on a single line
+    @wrap_create(pyversion="3.8", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], file=__file__)
+    def create_data(X=None):
+        import os
+        import datatable as dt
+        if X is not None and os.path.isfile(X):
+            X = dt.fread(X)
+        else:
+            X = None
+
+        my_path = os.path.dirname(__file__)
+
+        import pandas as pd
+        assert pd.__version__ == "1.1.5", "actual: %s" % pd.__version__
+
+        url = "http://data.un.org/_Docs/SYB/CSV/SYB63_226_202009_Net%20Disbursements%20from%20Official%20ODA%20to%20Recipients.csv"
+        import urllib.request
+
+        new_file = os.path.join(my_path, "user_file.csv")
+        urllib.request.urlretrieve(url, new_file)
+
+        import datatable as dt
+        return dt.fread(new_file)
