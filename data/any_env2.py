114880d6e273316f836fde3fe7c871bf89b87b9b
Jonathan C. McKinney <pseudotensor@gmail.com>
2021-05-21 12:27:12 -0700

    Add some comments

--- data/any_env2.py
+++ data/any_env2.py
@@ -26,6 +26,8 @@ def wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True
 def create_data_popen(func, *args, pyversion="3.6", install_h2oaicore=False, install_datatable=True,
                       modules_needed_by_name=[], cache_env=False, file=None, id=None,
                       X=None, **kwargs):
+    """ Run recipe in popen in isolated env
+    """
     print(
         "Recipe %s running pyversion=%s install_h2oaicore=%s install_datatable=%s modules_needed_by_name=%s cache_env=%s id=%s" % (
         file, pyversion, install_h2oaicore, install_datatable,
@@ -111,7 +113,9 @@ class FreshEnvData(CustomData):
     # Specify the python package dependencies.  Will be installed in order of list
     # NOTE: Keep @wrap_create on a single line
     # NOTE: If want to share cache across recipes, can set cache_env=True and set id=<some unique identifier, like myrecipe12345>
+    # Below caches the env into "id" folder
     #@wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], cache_env=True, file=__file__, id="myrecipe12345")
+    # Below does not cache the env
     @wrap_create(pyversion="3.6", install_h2oaicore=False, install_datatable=True, modules_needed_by_name=["pandas==1.1.5"], file=__file__)
     def create_data(X=None):
         import os
