0e39d748eddf7d0fcd1c5f64e81f5f6f61967191
Navdeep Gill <mr.navdeepgill@gmail.com>
2021-06-08 13:30:42 -0700

    Add examples for MLI BYOR (#151)

---
+++ explainers/explainers/examples/example_custom_explanation_explainer.py
@@ -0,0 +1,68 @@
+# Copyright 2017-2021 H2O.ai, Inc. All rights reserved.
+from h2oaicore.mli.oss.byor.core.explainers import (
+    CustomDaiExplainer,
+    CustomExplainer,
+)
+from h2oaicore.mli.oss.byor.core.explanations import CustomExplanation
+from h2oaicore.mli.oss.byor.core.representations import (
+    TextCustomExplanationFormat,
+)
+
+
+class MyCustomExplanation(CustomExplanation):
+    """Example of a user defined explanation type."""
+
+    _explanation_type = "user-guide-explanation-example"
+    _is_global = True
+
+    def __init__(
+        self, explainer, display_name: str = None, display_category: str = None
+    ) -> None:
+        CustomExplanation.__init__(
+            self,
+            explainer=explainer,
+            display_name=display_name,
+            display_category=display_category,
+        )
+
+    def validate(self) -> bool:
+        return self._formats is not None
+
+
+class ExampleCustomExplanationExplainer(CustomExplainer, CustomDaiExplainer):
+
+    _display_name = "Example Custom Explanation Explainer"
+    _description = (
+        "Explainer example which shows how to define custom explanation."
+    )
+    _regression = True
+    _explanation_types = [TextCustomExplanationFormat]
+
+    def __init__(self):
+        CustomExplainer.__init__(self)
+        CustomDaiExplainer.__init__(self)
+
+    def setup(self, model, persistence, **e_params):
+        CustomExplainer.setup(self, model, persistence, **e_params)
+        CustomDaiExplainer.setup(self, **e_params)
+
+    def explain(self, X, y=None, explanations_types=None, **kwargs) -> list:
+        df = X[:1, self.used_features]
+        prediction = self.model.predict_method(df)
+
+        # create CUSTOM explanation
+        explanation = MyCustomExplanation(
+            explainer=self,
+            display_name="Custom Explanation Example",
+            display_category="Example",
+        )
+        # add a text format to CUSTOM explanation
+        explanation.add_format(
+            TextCustomExplanationFormat(
+                explanation=explanation,
+                format_data=f"Prediction is: {prediction}",
+                format_file=None,
+            )
+        )
+
+        return [explanation]
---
+++ explainers/explainers/examples/example_hello_world_explainer.py
@@ -0,0 +1,23 @@
+# Copyright 2017-2021 H2O.ai, Inc. All rights reserved.
+from h2oaicore.mli.oss.byor.core.explainers import (
+    CustomExplainer,
+)
+from h2oaicore.mli.oss.byor.core.explanations import WorkDirArchiveExplanation
+
+
+class ExampleHelloWorldExplainer(CustomExplainer):
+
+    _display_name = "Hello, World!"
+    _description = "This is 'Hello, World!' explainer example."
+    _regression = True
+    _explanation_types = [WorkDirArchiveExplanation]
+
+    def __init__(self):
+        CustomExplainer.__init__(self)
+
+    def explain(self, X, y=None, explanations_types=None, **kwargs) -> list:
+        explanation = self.create_explanation_workdir_archive(
+            display_name=self.display_name, display_category="Demo"
+        )
+
+        return [explanation]
