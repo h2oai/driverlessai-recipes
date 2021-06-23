88d57f88f2ef6faf9510402ea52c85716c9b558d
Gregory Kanevsky <grigory93@gmail.com>
2021-06-23 15:29:58 -0500

    auto parse columns into column groups to aggregate

--- livecode/compute_rowwise_stats_by_column_groups.py
+++ livecode/compute_rowwise_stats_by_column_groups.py
@@ -6,24 +6,52 @@
 # Inputs:
 #   X: datatable - primary data set with one or more groups of time series columns
 # Parameters:
-#   columns: list of column name prefixes - each column prefix defines a column group to compute rowwise aggregates:
+#   columns: (optional) list of column name prefixes - each column prefix defines a column group to compute rowwise aggregates:
 #            sum, mean, standard deviation, max, min, rowfirst, rowlast. E.g Kaggle Credit Card Default dataset column
 #            names are "PAY_AMT", "BILL_AMT", and "PAY_"
-#   ranges: pairs of values that defines range 'from' and 'to' corresponding to each column group,
+#   ranges: (optional) pairs of values that defines range 'from' and 'to' corresponding to each column group,
 #           e.g. Kaggle Credit Card Default dataset ranges could be (1, 6), (1, 6), (2, 6)
 #           Currently, only integer enumerated column groups are supported.
+#   black_listed_columns: (optional) list of column name prefixes to exclude from aggregation (only applicable when neither
+#           'columns' nor 'ranges' provided to be parsed automatically
+#   min_col_group_size: minimal number of columns in column group to be aggregated, 2 by default.
 # Output:
 #   dataset augmented with computed rowwise statistics for each column group
 from datatable import f, update, rowsum, rowmean, rowsd, rowmax, rowmin, rowfirst, rowlast, rowcount
+import re
+from collections import defaultdict
 
-columns = ["PAY_AMT", "BILL_AMT", "PAY_"]
-ranges = [(1, 6), (1, 6), (2, 6)]
+columns = None # columns = ["PAY_AMT", "BILL_AMT", "PAY_"]
+ranges = None # [(1, 6), (1, 6), (2, 6)]
+black_listed_columns = []
+min_col_group_size = 2
 
-ts_col_lists = [[(col + "%d") % (week) for week in range(desde, hasta)] for (col, (desde, hasta)) in
-                zip(columns, ranges)]
-ts_col_slices = [((col + "%d") % (desde), (col + "%d") % (hasta)) for (col, (desde, hasta)) in zip(columns, ranges)]
+# parse column names for time series column groups
+if columns is None or columns == [] or\
+        ranges is None or ranges == []:
+    # match any column names that consist of alpha name (prefix) followed by integer index (suffix)
+    p = re.compile(r"^([a-zA-Z_]+)(\d+)$")
+    matches = [p.match(c) for c in X.names]
+    all_col_groups = defaultdict(list)
+    for m in matches:
+        if m is not None:
+            key = m.group(1)
+            val = int(m.group(2))
+            all_col_groups[key].append(val)
 
-for c, r, s in zip(columns, ranges, ts_col_slices):
+    # remove black listed columns or column groups that smaller than minimal size
+    col_groups= { key: val for key, val in all_col_groups.items() if not key in black_listed_columns or
+                  len(val) >= min_col_group_size}
+
+    # list of column prefixes
+    columns = list(col_groups.keys())
+    # list of column ranges
+    ranges = [(min(idx), max(idx)) for idx in col_groups.values()]
+
+# produce tuple for column slices
+col_slices = [((col + "%d") % (desde), (col + "%d") % (hasta)) for (col, (desde, hasta)) in zip(columns, ranges)]
+
+for c, r, s in zip(columns, ranges, col_slices):
     update_map = {c + "_sum": rowsum(f[s[0]:s[1]]),
                   c + "_mean": rowmean(f[s[0]:s[1]]),
                   c + "_sd": rowsd(f[s[0]:s[1]]),
