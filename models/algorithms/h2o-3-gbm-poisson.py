508663ed6bd1961925fcb29fde268493fe40e9e8
Achraf Merzouki <achraf.merzouki@h2o.ai>
2021-12-29 15:26:04 -0500

    Upgrade h2o-3 to 3.34.0.7

--- models/algorithms/h2o-3-gbm-poisson.py
+++ models/algorithms/h2o-3-gbm-poisson.py
@@ -8,7 +8,7 @@ from h2o.estimators.gbm import H2OGradientBoostingEstimator
 
 import numpy as np
 
-_global_modules_needed_by_name = ['h2o==3.34.0.3']
+_global_modules_needed_by_name = ['h2o==3.34.0.7']
 import h2o
 import os
 
