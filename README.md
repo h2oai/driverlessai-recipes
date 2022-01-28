3b06c4e7ed3f633991b09cecd5290cde06b1f0a2
Arno Candel <arno.candel@gmail.com>
2022-01-28 10:38:47 -0800

    Add isolation forest anomaly with custom scorer.

--- README.md
+++ README.md
@@ -81,7 +81,7 @@ abled.
  [1.10.0](https://github.com/h2oai/driverlessai-recipes/tree/rel-1.10.0)
  [1.10.1](https://github.com/h2oai/driverlessai-recipes/tree/rel-1.10.1)
  [1.10.2](https://github.com/h2oai/driverlessai-recipes/tree/rel-1.10.2)
-### Count: 245
+### Count: 246
 * [AIR-GAPPED_INSTALLATIONS](./air-gapped_installations)
   * [load_custom_recipe.py](./air-gapped_installations/load_custom_recipe.py) [please add description] 
 * [DATA](./data)
@@ -236,6 +236,7 @@ abled.
     * [historic_mean.py](./models/timeseries/historic_mean.py) [Historic Mean for Time-Series problems. Predicts the mean of the target for each timegroup for regression problems.] 
   * [UNSUPERVISED](./models/unsupervised)
     * [dbscan.py](./models/unsupervised/dbscan.py) [Clustering using DBScan] 
+    * [isolation_forest_custom_scorer.py](./models/unsupervised/isolation_forest_custom_scorer.py) [Custom Anomaly Score for Isolation Forest] 
     * [local_outlier_factor.py](./models/unsupervised/local_outlier_factor.py) [Outlier detection with Local Outlier Factor] 
     * [rapids_kmeans.py](./models/unsupervised/rapids_kmeans.py) [KMeans clustering using RAPIDS.ai] 
   * [UPLIFT](./models/uplift)
---
+++ models/unsupervised/isolation_forest_custom_scorer.py
@@ -0,0 +1,34 @@
+"""Custom Anomaly Score for Isolation Forest"""
+import numpy as np
+from h2oaicore.metrics import CustomUnsupervisedScorer
+from h2oaicore.models import CustomUnsupervisedModel
+from h2oaicore.models_unsupervised import IsolationForestAnomalyModel
+
+
+class MyAnomalyScorer(CustomUnsupervisedScorer):
+    _perfect_score = 1e30
+    _maximize = True
+
+    def score(self, actual, predicted, sample_weight=None, labels=None, X=None, **kwargs):
+        # Custom scorer for anomaly scores to allow unsupervised AutoML.
+
+        # Idea: just return the variance of the predicted anomaly scores
+
+        # This blindly assumes that higher variance in anomaly score is better.
+        # WARNING: This is a strong assumption, and might not be true or useful.
+
+        # Please customize using your domain knowledge!
+        # Try to gain insight into anomaly score distributions first, before making a scorer.
+        # X is provided here. E.g., one can do aggregations of anomaly scores across specific cohorts.
+        return np.var(predicted)
+
+class MyIsolationForestAnomalyModel(IsolationForestAnomalyModel, CustomUnsupervisedModel):
+    _included_scorers = ['MyAnomalyScorer']
+
+    # Feature Engineering is controlled by the pretransformers (one-time, upfront).
+    # Can use any custom transformer that takes raw input and returns numeric data, anything goes.
+    # By default, categorical features are converted to numerics using frequency encoding.
+    # By default, dates/text/images are dropped. But this can be changed arbitrarily.
+
+    # _included_pretransformers = ['OrigFreqPreTransformer']  # default - pass-through numerics, frequency-encode categoricals, drop rest
+    # _included_pretransformers = ['OrigOHEPreTransformer']   # option  - pass-through numerics, one-hot-encode categoricals, drop rest
