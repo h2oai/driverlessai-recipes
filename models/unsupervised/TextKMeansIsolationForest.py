6763bdb44dd783a45883bd28bd6fac85264c6d0b
Jonathan C. McKinney <pseudotensor@gmail.com>
2022-11-03 12:19:19 -0700

    Avoid trying to grab recipe separately, works, but harder to know what recipes needed for export for another user

--- models/unsupervised/TextKMeansIsolationForest.py
+++ models/unsupervised/TextKMeansIsolationForest.py
@@ -85,7 +85,7 @@ class NumCatTxtPreTransformer(CustomUnsupervisedTransformer):
     _only_as_pretransformer = True
     _num = ("StandardScalerTransformer", "numeric")
     _cat = ("FrequentTransformer", "categorical")
-    _txt = ("TextLDATopicTransformer", "text")
+    _txt = ("TextLDATopicUnsupervisedTransformer", "text")
 
     @staticmethod
     def get_default_properties():
@@ -143,8 +143,7 @@ class NumCatTxtPreTransformer(CustomUnsupervisedTransformer):
             self.cat_cols = col_dict[self._cat[1]]
             self.cat_cols = sorted(set([x for x in self.cat_cols if x in set_all_cols]))
         if self._txt:
-            module_name = self.get_text_module_name()
-            TxtTrans = getattr(importlib.import_module(module_name), self._txt[0])
+            TxtTrans = TextLDATopicUnsupervisedTransformer
             self.txt_cols = col_dict[self._txt[1]]
             self.txt_cols = sorted(set([x for x in self.txt_cols if x in set_all_cols]))
 
@@ -172,37 +171,6 @@ class NumCatTxtPreTransformer(CustomUnsupervisedTransformer):
             self.union = [([x], CatTrans(cat_cols=[x], input_feature_names=[x], **kwargs)) for x in self.cat_cols]
         elif self.num_cols and NumTrans:
             self.union = [([x], NumTrans(num_cols=[x], input_feature_names=[x], **kwargs)) for x in self.num_cols]
-
-    @staticmethod
-    def get_text_module_name():
-        name = 'transformers'
-        self = NumCatTxtPreTransformer
-        from h2oaicore.utils import ContribLoader
-        # 'text_preprocessing' is pattern matching name of file for custom recipe we wish to use
-        module_names = [k for k, v in sys.modules.items() if
-                        '%s%s' % (ContribLoader._contentpattern, name) in k and self._txt[0] in dir(v)]
-        if name in config.recipe_activation and self._txt[0] in config.recipe_activation[name]:
-            file_name = config.recipe_activation[name][self._txt[0]].split('|')[1]
-            module_names = [x for x in module_names if file_name in x]
-            assert len(module_names) > 0, "Missing %s recipe, please upload" % self._txt[0]
-            assert len(module_names) == 1, "Unexpected multiple matches for %s recipe" % self._txt[0]
-            module_name = module_names[0]
-        else:
-            # for acceptance testing, unsure how else to know which one to use unless specify exact match
-            assert len(module_names) > 0, "Missing %s recipe," \
-                                          " please upload (config.recipe_activation=%s)" % \
-                                          (self._txt[0], config.recipe_activation)
-            module_name = module_names[-1]  # use latest one.
-        return module_name
-
-    def file_deps(self):
-        """
-        Get dependent modules that aren't directly top-level used by DAI
-        :return:
-        """
-        module_name = self.get_text_module_name()
-        file_name = os.path.abspath(sys.modules[module_name].__file__)
-        return [file_name]
 
     def transform(self, X, y=None, **fit_params):
         frames = []
@@ -238,3 +206,52 @@ class StdFreqTextPreTransformer(NumCatTxtPreTransformer):
 class OrigOHETxtPreTransformer(NumCatTxtPreTransformer):
     _num = ("OriginalTransformer", "numeric")
     _cat = ("OneHotEncodingUnsupervisedTransformer", "ohe_categorical")
+
+
+class TextLDATopicUnsupervisedTransformer(CustomUnsupervisedTransformer):
+    _unsupervised = True
+
+    """Transformer to extract topics from text column using LDA"""
+    _is_reproducible = False
+    _testing_can_skip_failure = False  # ensure tested as if shouldn't fail
+    _modules_needed_by_name = ["gensim==3.8.0"]
+
+    def __init__(self, n_topics, **kwargs):
+        super().__init__(**kwargs)
+        self.n_topics = n_topics
+
+    @staticmethod
+    def get_default_properties():
+        return dict(col_type="text", min_cols=1, max_cols=1, relative_importance=1)
+
+    @staticmethod
+    def get_parameter_choices():
+        return {"n_topics": [3, 5, 10, 50]}
+
+    def fit_transform(self, X: dt.Frame, y: np.array = None):
+        import gensim
+        from gensim import corpora
+        X = dt.Frame(X)
+        new_X = X.to_pandas().astype(str).fillna("NA").iloc[:, 0].values
+        new_X = [doc.split() for doc in new_X]
+        self.dictionary = corpora.Dictionary(new_X)
+        new_X = [self.dictionary.doc2bow(doc) for doc in new_X]
+        self.model = gensim.models.ldamodel.LdaModel(new_X,
+                                                     num_topics=self.n_topics,
+                                                     id2word=self.dictionary,
+                                                     passes=10,
+                                                     random_state=2019)
+        return self.transform(X)
+
+    def transform(self, X: dt.Frame):
+        X = dt.Frame(X)
+        orig_col_name = X.names[0]
+        new_X = X.to_pandas().astype(str).fillna("NA").iloc[:, 0].values
+        new_X = [doc.split() for doc in new_X]
+        new_X = [self.dictionary.doc2bow(doc) for doc in new_X]
+        new_X = self.model.inference(new_X)[0]
+        self._output_feature_names = [f'{self.display_name}{orig_feat_prefix}{orig_col_name}{extra_prefix}topic{i}'
+                                      for i in range(new_X.shape[1])]
+        self._feature_desc = [f'LDA Topic {i} of {self.n_topics} for {orig_col_name} column' for i in
+                              range(new_X.shape[1])]
+        return new_X
