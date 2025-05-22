394d92338d359d1118f0fa182fae40b3f9118746
Sujeevan Rasaratnam <sujeevan.rasaratnam@h2o.ai>
2025-05-22 14:59:46 +0000

    fix resource not found error

--- transformers/nlp/text_pos_tagging_transformer.py
+++ transformers/nlp/text_pos_tagging_transformer.py
@@ -17,20 +17,22 @@ class POSTagTransformer:
 
     def set_tagger(self):
         import nltk
+        from nltk.tokenize import word_tokenize
+
         nltk_data_path = os.path.join(user_dir(), config.contrib_env_relative_directory, "nltk_data")
         nltk_temp_path = os.path.join(user_dir(), "nltk_data")
         nltk.data.path.append(nltk_data_path)
-        nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_path)
+        nltk.download('averaged_perceptron_tagger_eng', download_dir=nltk_data_path)
         try:
             self.pos_tagger = nltk.pos_tag
-            self.pos_tagger(list("test"))
+            self.pos_tagger(word_tokenize("test"))
         except LookupError:
             os.makedirs(nltk_data_path, exist_ok=True)
             os.makedirs(nltk_temp_path, exist_ok=True)
             tagger_path = os.path.join(nltk_data_path, "taggers")
             os.makedirs(tagger_path, exist_ok=True)
             file1 = download(
-                "https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/taggers/averaged_perceptron_tagger.zip",
+                "https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/taggers/averaged_perceptron_tagger_eng.zip",
                 dest_path=nltk_temp_path)
             file2 = download(
                 "https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/taggers/maxent_treebank_pos_tagger.zip",
@@ -40,7 +42,7 @@ class POSTagTransformer:
             self.atomic_copy(file1, tagger_path)
             self.atomic_copy(file2, tagger_path)
             self.pos_tagger = nltk.pos_tag
-            self.pos_tagger(list("test"))
+            self.pos_tagger(word_tokenize("test"))
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
