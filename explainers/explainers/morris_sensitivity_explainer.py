81d02fe73fcecc105842c17fe98fb30fb792d15b
jackjii79 <peterhan1987@gmail.com>
2026-09-21 12:05:53 -0700

    upgrade interpret to 0.7.8 (#257)

--- explainers/explainers/morris_sensitivity_explainer.py
+++ explainers/explainers/morris_sensitivity_explainer.py
@@ -48,7 +48,7 @@ class MorrisSensitivityLeExplainer(CustomExplainer, CustomDaiExplainer):
     # declaration of explanation types this explainer creates e.g. feature importance
     _explanation_types = [GlobalFeatImpExplanation]
     # Python package dependencies (can be installed using pip)
-    _modules_needed_by_name = ["interpret==0.6.1"]
+    _modules_needed_by_name = ["interpret==0.7.8"]
 
     # explainer constructor must not have any required parameters
     def __init__(self):
